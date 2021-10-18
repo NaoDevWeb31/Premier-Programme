997bd6846ed06bcdfec84cfbf85d2814ae7956c9
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:03:08 +0200

    [MAJ] Concaténation

--- exemple.js
+++ exemple.js
@@ -1,21 +1,8 @@
 //                  LES VARIABLES & LES OPÉRATEURS
 
-// let prenom = "Marc & Simon"
-// // Chaînes de caractères (string)
+const prenom = "Naomi";
+const nom = "Mango";
 
-// let age = 21.5;
-// // Nombre (number)
+console.log("Bonjour " + prenom + " " + nom + " !");
 
-// let majorite = true;
-// // Booleen (boolean)
-
-// let win = false;
-// win = true;
-
-// const prenom = "Marc"; // impossible de modifier
-let prenom = "Marc";
-prenom = "John";
-
-console.log(prenom);
-
-// var prenom = "Marc";
+// Bonjour [prenom] [age] !
