a6d1a4fb162ca4840e2c4dfcb098bf8d5a010f40
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:07:59 +0200

    [MAJ] Boite de dialogue - afficher

--- exemple.js
+++ exemple.js
@@ -1,11 +1,3 @@
-//                  LES VARIABLES & LES OPÉRATEURS
+//                  DEMANDER DES INFOS AUX UTILISATEURS
 
-let nombreUn = 8,
-  nombreDeux = 4,
-  result;
-
-result = (nombreUn + nombreDeux) * 9; // 108
-result++; // incrémentation => ajouter 1
-result--; // décrémentation => retirer 1
-
-console.log(result);
+alert("Hello World !");
