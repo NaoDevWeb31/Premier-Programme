ba3979e5ece4fcd0a3b8f6f51bce1d02654ed059
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:42:42 +0200

    [MAJ] Convertir les données v4

--- exemple.js
+++ exemple.js
@@ -3,7 +3,8 @@
 function prevoirAge() {
   let age = prompt("Quel est votre âge ?");
 
-  age = parseInt(age); // Convertir une chaine en nombre entier
+  // age = parseInt(age); // Convertir une chaine en nombre entier
+  age = Number(age); // Méthode RECOMMANDÉE pour convertir une chaine en nombre sans se prendre la tête
 
   alert("Bientôt, vous aurez " + (age + 1) + " !"); // "30" + 1 = 301
 }
