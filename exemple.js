3819ffaf8c4ddecd0db3a7eb38b887292ae54275
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:47:12 +0200

    [MAJ] Fonction anonyme v1

--- exemple.js
+++ exemple.js
@@ -1,17 +1,5 @@
 //                  LES FONCTIONS
 
-function prevoirAge() {
-  let age = prompt("Quel est votre âge ?");
-
-  // age = parseInt(age); // Convertir une chaine en nombre entier
-  age = parseFloat(age); // Convertir une chaine en nombre décimal
-  // age = Number(age); // Méthode RECOMMANDÉE pour convertir une chaine en nombre sans se prendre la tête
-
-  // nombre = 45;
-  // nombreEnString = nombre + ""; // Méthode "barbare" pour convertir un nombre en chaine;
-  // nombreEnString = nombre.toString(); // Convertir un nombre en chaine
-
-  alert("Bientôt, vous aurez " + (age + 1) + " !");
+function(){
+    console.log("Je suis une fonction anonyme");
 }
-
-prevoirAge();
