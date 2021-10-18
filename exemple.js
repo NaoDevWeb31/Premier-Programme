9be49399000cfa4e296a3262cb402e38f421d36a
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:45:39 +0200

    [MAJ] Convertir les données v7

--- exemple.js
+++ exemple.js
@@ -8,7 +8,8 @@ function prevoirAge() {
   // age = Number(age); // Méthode RECOMMANDÉE pour convertir une chaine en nombre sans se prendre la tête
 
   // nombre = 45;
-  // nombreEnString = nombre + ""; // Méthode "barbare" pour convertir un nombre en chaine
+  // nombreEnString = nombre + ""; // Méthode "barbare" pour convertir un nombre en chaine;
+  // nombreEnString = nombre.toString(); // Convertir un nombre en chaine
 
   alert("Bientôt, vous aurez " + (age + 1) + " !");
 }
