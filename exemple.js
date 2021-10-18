8dc6c99d0690b9bd6901979aa359f2a9b9e3a2ee
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:54:26 +0200

    [MAJ] Fonctions - Exercice #1

--- exemple.js
+++ exemple.js
@@ -1,5 +1,19 @@
-//                  LES FONCTIONS
+//                  EXERCICES SUR LES FONCTIONS
 
-(function () {
-  console.log("Je suis une fonction anonyme");
-})();
+// Exercice #1 : Débutant
+function abracadabra() {
+  let prenom = prompt("Quel est votre prénom ?");
+  let nom = prompt("Quel est votre nom ?");
+  let age = prompt("Quel est votre âge ?");
+  alert(
+    "Sapristi ! On ne m'avait pas prévenu que c'était vous, " +
+      prenom +
+      " ! Euh... Je veux dire... Monsieur le grand magicien " +
+      nom +
+      " ! Cela fait déjà " +
+      age +
+      " ans que vous faites rayonner notre contrée !"
+  );
+}
+
+abracadabra();
