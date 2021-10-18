5dc219d08eb371c9175bc3eee76ba801c1052b5b
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:01:07 +0200

    [MAJ] Premières variables

---
+++ exemple.js
@@ -0,0 +1,21 @@
+//                  LES VARIABLES & LES OPÉRATEURS
+
+// let prenom = "Marc & Simon"
+// // Chaînes de caractères (string)
+
+// let age = 21.5;
+// // Nombre (number)
+
+// let majorite = true;
+// // Booleen (boolean)
+
+// let win = false;
+// win = true;
+
+// const prenom = "Marc"; // impossible de modifier
+let prenom = "Marc";
+prenom = "John";
+
+console.log(prenom);
+
+// var prenom = "Marc";
