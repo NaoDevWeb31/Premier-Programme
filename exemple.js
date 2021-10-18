67764b423e2ac4cf20fc4bea3252cd9d41b18475
Naomi Mango <naomi.mango@gmail.com>
2021-10-18 17:27:39 +0200

    [MAJ] Paramètres par défaut v1

--- exemple.js
+++ exemple.js
@@ -3,21 +3,9 @@
 let nombreUn = 4;
 nombreDeux = 7;
 
-function addition(nombreA, nombreB) {
+function addition(nombreA, nombreB = 10) {
   let result = nombreA + nombreB;
-  // console.log(result);
-  return result; // 11
+  return result;
 }
 
-function division(nombreA, nombreB) {
-  let result = nombreA / nombreB;
-  // console.log(result);
-  return result; // 0.5714285714285714
-}
-
-let resultatAddition = addition(nombreUn, nombreDeux);
-let resultatDivision = division(nombreUn, nombreDeux);
-
-console.log(resultatAddition);
-console.log(resultatDivision);
-console.log(resultatAddition * resultatDivision); // 6.285714285714286
+console.log(addition(nombreUn)); // 14
