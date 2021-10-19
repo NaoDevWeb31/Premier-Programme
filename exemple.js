8547fd5bbcb5c34afc987b041aaaea7d35a89563
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 12:42:49 +0200

    [MAJ] Conditions - switch v2

--- exemple.js
+++ exemple.js
@@ -5,4 +5,9 @@ let consommable = "carotte";
 switch (consommable) {
   case "carotte":
     console.log("Ceci est un légume.");
+    break; // sortir de la condition
+
+  case "banane":
+    console.log("Ceci est un fruit.");
+    break; // CONSEILLÉ d'en mettre à chaque sortie de condition
 }
