9027fe6002ed90043ecfc242dd757fb54c21275a
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 12:55:53 +0200

    [MAJ] Conditions - OR/AND v4

--- exemple.js
+++ exemple.js
@@ -3,8 +3,8 @@
 let gareDeDepart = "Valenciennes";
 let gareDArrivee = "";
 
-// AND
-if (gareDeDepart != "" && gareDArrivee != "") {
+// OR (double pipe)
+if (gareDeDepart != "" || gareDArrivee != "") {
   console.log("Le train va démarrer.");
 } else {
   console.log("Le train ne peut pas démarrer.");
