0b336bd4982c08c217277780d396ab6ca9de74f1
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 14:00:50 +0200

    [MAJ] Boucles - while

--- exemple.js
+++ exemple.js
@@ -1,27 +1,10 @@
-//                  LES CONDITIONS
+//                  LES BOUCLES
 
-let gareDeDepart = "Valenciennes";
-let gareDArrivee = prompt("Où souhaitez-vous aller ?")
-                    /* Création d'une valeur par défaut => */
-                    || "Gare du Nord";
-let chauffeur = "Naomi";
+let i = 1;
 
-if ((gareDeDepart != "" || gareDArrivee != "") && chauffeur != "") {
-  alert("Le train va démarrer à destination de " + gareDArrivee + ".");
-} else {
-  alert("Le train ne peut pas démarrer.");
+while (i < 5) {
+
+  console.log("Ligne : " + i);
+  i++;
+  
 }
-
-/****************************************************************************************************/
-
-// let x = 5;
-
-// if (x > 3) {
-//   console.log("x est supérieur à trois");
-// } else {
-//   console.log("x est inférieur à trois");
-// }
-
-// x > 3
-//   ? console.log("x est supérieur à trois")
-//   : console.log("x est inférieur à trois");
