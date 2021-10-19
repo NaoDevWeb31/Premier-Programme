f13e74f0a0eeeb455f5ae850cdc6289382e47086
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 12:44:18 +0200

    [MAJ] Conditions - switch v3

--- exemple.js
+++ exemple.js
@@ -1,13 +1,14 @@
 //                  LES CONDITIONS
 
-let consommable = "carotte";
+let consommable = "pomme de terre";
 
 switch (consommable) {
   case "carotte":
+  case "pomme de terre": // possible d'en placer les uns à la suite des autres
     console.log("Ceci est un légume.");
     break; // sortir de la condition
 
   case "banane":
     console.log("Ceci est un fruit.");
-    break; // CONSEILLÉ d'en mettre à chaque sortie de condition
+    break;
 }
