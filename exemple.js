26b02107fd421886288d8bb4bdfd3f5a46235899
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 12:12:00 +0200

    [MAJ] Conditions - if/else v1

--- exemple.js
+++ exemple.js
@@ -1,20 +1,7 @@
-//                  EXERCICES SUR LES FONCTIONS
+//                  LES CONDITIONS
 
-// Exercice #2 : Avancé
-// // La solution
-function solutionCalculerIMC(poids, taille) {
-  // Convertir la taille en mètres
-  let tailleEnMetres = taille / 100;
-  // Calcule la taille à la puissance 2
-  let tailleCalculee = Math.pow(tailleEnMetres, 2); // tailleEnMetres²
-  // Calcule l'IMC
-  let resultat = poids / tailleCalculee;
-  return resultat;
+let heure = 11;
+
+if (true) {
+  console.log("Je suis dans la condition");
 }
-
-poids = prompt("Renseignez votre poids en kg :");
-taille = prompt("Renseignez votre taille en cm :");
-alert(
-  "Votre indice de masse corporelle est de " +
-    solutionCalculerIMC(poids, taille)
-);
