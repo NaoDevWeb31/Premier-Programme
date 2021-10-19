fa0852eda77a0fb96a748734726246ef5aa91512
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 14:25:56 +0200

    [MAJ] Boucles - Casser avec break & continue v3

--- exemple.js
+++ exemple.js
@@ -6,9 +6,10 @@ while (i < 5) {
 
   // Si i est égal à 3, la boucle s'arrête
   if (i == 3) {
-    break;
+    i++;
+    continue; // la ligne 3 n'est pas exécutée => passe à la prochaine itération
   }
-    
+
   // Tant que i est inférieur à 5, afficher...
   console.log("Ligne : " + i);
   // i + 1
