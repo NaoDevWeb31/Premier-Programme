8b65e5904b15cdff9786b7ac456df8cf9f285b38
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 12:40:22 +0200

    [MAJ] Conditions - switch v1

--- exemple.js
+++ exemple.js
@@ -1,21 +1,8 @@
-//                  LES CONDITIONS - Fiche technique sur les opérateurs de comparaison
+//                  LES CONDITIONS
 
-// == : égal à la valeur
-// === : égal à la valeur et au type
-// != : différent de la valeur
-// !== : différent de la valeur et du type
-// > : supérieur
-// < : inférieur
-// >= : supérieur ou égal
-// <= : inférieur ou égal
+let consommable = "carotte";
 
-// Si nous prenons un exemple, voici la différence entre l'opérateur == et === :
-let nombre = 3;
-
-if (nombre == "3") {
-  // vaudra "true"
-}
-if (nombre === "3") {
-  // vaudra "false"
-  // nombre est de type Number
+switch (consommable) {
+  case "carotte":
+    console.log("Ceci est un légume.");
 }
