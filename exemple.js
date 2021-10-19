06a09b6a427f99456e8949f29ae11f0c41e9c4f7
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 14:35:47 +0200

    [MAJ] Boucles - Exceptions avec try & catch v1

--- exemple.js
+++ exemple.js
@@ -1,18 +1,11 @@
 //                  LES BOUCLES
 
-let i = 0;
+try {
 
-while (i < 5) {
+    // Erreur ?
 
-  // Si i est égal à 3, la boucle s'arrête
-  if (i == 3) {
-    i++;
-    continue; // la ligne 3 n'est pas exécutée => passe à la prochaine itération
-  }
-
-  // Tant que i est inférieur à 5, afficher...
-  console.log("Ligne : " + i);
-  // i + 1
-  i++;
-
+} catch {
+    
+    // Erreur !
+    
 }
