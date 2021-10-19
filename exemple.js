49ca4dee1fc7b498e16b7d6c0a344e6e28d06f70
Naomi Mango <naomi.mango@gmail.com>
2021-10-19 12:33:15 +0200

    [MAJ] Conditions - if/else v7

--- exemple.js
+++ exemple.js
@@ -1,15 +1,15 @@
-//                  LES CONDITIONS - Challenge
+//                  LES CONDITIONS - Challenge - Solution
 
 // age
 // -18 : Vous n'êtes pas majeur
 // 18 et 20 : Vous êtes majeur en France
 // 21+ : Vous êtes majeur partout, à vous les casinos !
 
-let age; // Tester avec différents âges
+let age = 40;
 
 if (age < 18) {
   console.log("Vous n'êtes pas majeur.");
-} else if (age <= 20) {
+} else if (age < 21) {
   console.log("Vous êtes majeur en France.");
 } else {
   console.log("Vous êtes majeur partout, à vous les casinos !");
