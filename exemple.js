ecda50220c894df75a606fe858e15e64a2898177
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 11:51:06 +0200

    [MAJ] Tableaux - Ajout d'un élément v1

--- exemple.js
+++ exemple.js
@@ -1,26 +1,13 @@
 //                  LES TABLEAUX
 
-// let monTableau = new Array("un", "deux", "trois");
-// let monTableau = Array("un", "deux", "trois");
-let monTableau = ["un", "deux", "trois"]; // Désormais
+// Tableau simple
+let monTableau = ["un", "deux", "trois", "quatre"];
 
-// Accéder au dernier élément du tableau
-console.log(monTableau[monTableau.length - 1]); // trois
-
-// Ancienne version - Tableau multidimensionnel
-// let monTableau2D = new Array(
-//   Array("Mark", "Jeff", "Bill"),
-//   Array("Zuckerberg", "Bezos", "Gates")
-// );
-
-// Nouvelle version - Tableau multidimensionnel
+// Tableau dimensionnel
 let monTableau2D = [
   ["Mark", "Jeff", "Bill"],
   ["Zuckerberg", "Bezos", "Gates"],
 ];
-
-// Accéder à un élément d'un tableau multidimensionnel
-console.log(monTableau2D[0][2]); // Mark
 
 // Tableau associatif
 let monTableauAssociatif = {
@@ -28,6 +15,3 @@ let monTableauAssociatif = {
   nom: "Zuckerberg",
   poste: "PDG de Facebook",
 };
-
-// Accéder à un élément d'un tableau associatif
-console.log(monTableauAssociatif["poste"]); // PDG de Facebook
