11c8f8a125583bff258c09ce6dc6d38ecc26f8dd
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 16:49:30 +0200

    [MAJ] Opérateur spread v4

--- exemple.js
+++ exemple.js
@@ -3,6 +3,13 @@
 
 // Exemple 1 : fusionner plusieurs tableaux
 let fruits = ["fraise", "banane", "poire"];
-let aliments = ["chocolat", "sucre", "lait", ...fruits]; // ... décompose les éléments du tableau "fruits" un à un et les ajoute au tableau "aliments"
+// Décomposer les éléments du tableau "fruits" un à un et les ajoute au tableau "aliments"
+let aliments = ["chocolat", "sucre", "lait", ...fruits];
 
-console.log(aliments); // ['chocolat', 'sucre', 'lait', 'fraise', 'banane', 'poire']
+// console.log(aliments); // ['chocolat', 'sucre', 'lait', 'fraise', 'banane', 'poire']
+
+// Exemple 2 : découper une chaîne de caractères en plusieurs éléments
+let phrase = "Bonjour !";
+let phraseTableau = [...phrase];
+
+console.log(phraseTableau); // ['B', 'o', 'n', 'j', 'o', 'u', 'r', ' ', '!']
