9c388dd253f0f2027821b94f261a1e4bf6502410
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 16:47:23 +0200

    [MAJ] Opérateur spread v3

--- exemple.js
+++ exemple.js
@@ -3,7 +3,6 @@
 
 // Exemple 1 : fusionner plusieurs tableaux
 let fruits = ["fraise", "banane", "poire"];
-let aliments = ["chocolat", "sucre", "lait"];
-let total = aliments.concat(fruits); // Combiner plusieurs tableaux
+let aliments = ["chocolat", "sucre", "lait", ...fruits]; // ... décompose les éléments du tableau "fruits" un à un et les ajoute au tableau "aliments"
 
-console.log(total); // ['chocolat', 'sucre', 'lait', 'fraise', 'banane', 'poire']
+console.log(aliments); // ['chocolat', 'sucre', 'lait', 'fraise', 'banane', 'poire']
