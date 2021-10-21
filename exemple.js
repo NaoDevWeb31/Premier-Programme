2e51ceaf84ba214409818f31b8cc33916bee08d1
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 16:01:16 +0200

    [MAJ] Objets - WeakSet v4

--- exemple.js
+++ exemple.js
@@ -1,7 +1,5 @@
 //                  LES OBJETS
 
-let voitures = new WeakSet(); // Créer un WeakSet => collection d'objets
-
 let voitureA = {
   constructeur: "Tesla",
   modele: "Cybertruck",
@@ -12,7 +10,6 @@ let voitureB = {
   modele: "Espace",
 };
 
-voitures.add(voitureA); // Ajouter un objet au WeakSet
-voitures.add(voitureB); // Ajouter un objet au WeakSet
+let voitures = new WeakSet([voitureA, voitureB]); // Créer un WeakSet en ajoutant les objets au WeakSet
 
 console.log(voitures); // WeakSet {{constructeur, …}, {constructeur, …}}
