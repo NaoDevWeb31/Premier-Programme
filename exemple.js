254e24672e5f9f6d0f2767f95e2323411ee43d1c
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 15:51:21 +0200

    [MAJ] Objets - Map v7

--- exemple.js
+++ exemple.js
@@ -16,3 +16,7 @@ utilisateurs.set(
 ); // Ajouter une paire clé-valeur à la Map "utilisateurs"
 
 console.log(utilisateurs); // {"Mark Zuckerberg" => {email: 'mark@facebook.com', poste: 'PDG'}}, Bill Gates => {email: 'bill@gatesnotes.com', poste: 'Sauver le monde'}}
+
+utilisateurs.delete("Bill Gates"); // Supprimer de la Map "utilisateurs" grâce à la clé
+
+console.log(utilisateurs); // {"Mark Zuckerberg" => {email: 'mark@facebook.com', poste: 'PDG'}}
