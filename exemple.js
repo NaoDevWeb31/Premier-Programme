f77627fd2fc20d5e9c1fb6268e727cd8ccb83026
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 15:07:04 +0200

    [MAJ] Objets - Création v3

--- exemple.js
+++ exemple.js
@@ -1,12 +1,24 @@
 //                  LES OBJETS
 
-let chien = {
-  race: "Shiba",
-  poil: "Court",
-  // Challenge
-  aboyer: function() {
-    console.log("Ouaf ouaf");
+// let chien = {
+//   race: "Shiba",
+//   poil: "Court",
+//   aboyer: function() {
+//     console.log("Ouaf ouaf");
+//   },
+// };
+
+let magicien = {
+  attaquer: function () {
+    console.log("Le magicien lance un sort");
   },
 };
 
-chien.aboyer(); // Ouaf ouaf
+let guerrier = {
+  attaquer: function () {
+    console.log("Le guerrier prend son épée");
+  },
+};
+
+magicien.attaquer(); // Le magicien lance un sort
+guerrier.attaquer(); // Le guerrier prend son épée
