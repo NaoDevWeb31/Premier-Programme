6a31faa82659e011f125dee2ff3939b03998cff4
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 12:38:08 +0200

    [MAJ] Tableaux - Concaténation d'un tableau v3

--- exemple.js
+++ exemple.js
@@ -2,16 +2,19 @@
 
 // Tableau simple
 let monTableau = ["un", "deux", "trois", "quatre"];
-console.log(monTableau.join()); // un,deux,trois,quatre
-console.log(monTableau.join(", ")); // un, deux, trois, quatre
-console.log(monTableau.join(" / ")); // un / deux / trois / quatre
+// console.log(monTableau.join()); // un,deux,trois,quatre
+// console.log(monTableau.join(", ")); // un, deux, trois, quatre
+// console.log(monTableau.join(" / ")); // un / deux / trois / quatre
 
 // Tableau dimensionnel
 let monTableau2D = [
   ["Mark", "Jeff", "Bill"],
   ["Zuckerberg", "Bezos", "Gates"],
 ];
-// console.log(monTableau2D);
+console.log(monTableau2D.join()); // Mark,Jeff,Bill,Zuckerberg,Bezos,Gates
+console.log(monTableau2D.join(", ")); // Mark,Jeff,Bill, Zuckerberg,Bezos,Gates
+console.log(monTableau2D[0].join(", ")); // Mark,Jeff,Bill
+console.log(monTableau2D.join(" / ")); // Mark,Jeff,Bill / Zuckerberg,Bezos,Gates
 
 // Tableau associatif
 let monTableauAssociatif = {
