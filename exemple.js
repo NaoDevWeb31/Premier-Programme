dfad25a3db14bf1335678aeaf6785ff7735abaaf
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 14:23:06 +0200

    [MAJ] Fonctions fléchées v3

--- exemple.js
+++ exemple.js
@@ -1,20 +1,11 @@
 //                  LES FONCTIONS FLÉCHÉES
 
-let maFonction = function() {
-  console.log("test");
-};
+// let maFonction = function() {
+//   console.log("test");
+// };
 
-// Sans paramètre
 let maFonction = () => {
-
-}
-// Un paramètre
-let maFonction = parametre => {
-
-}
-// Plusieurs paramètre
-let maFonction = (parametre, autreParametre) => {
-
+  console.log("test");
 }
 
 maFonction();
