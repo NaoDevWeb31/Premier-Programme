195543f03443095b76b850c401ee21d701b92281
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 16:57:23 +0200

    [MAJ] Opérateur spread v6

--- exemple.js
+++ exemple.js
@@ -2,24 +2,27 @@
 // // Opérateur de décomposition
 
 // Exemple 1 : fusionner plusieurs tableaux
-let fruits = ["fraise", "banane", "poire"];
-// Décomposer les éléments du tableau "fruits" un à un et les ajoute au tableau "aliments"
-let aliments = ["chocolat", "sucre", "lait", ...fruits];
+  let fruits = ["fraise", "banane", "poire"];
+  // Décomposer les éléments du tableau "fruits" un à un et les ajoute au tableau "aliments"
+  let aliments = ["chocolat", "sucre", "lait", ...fruits];
 
-// console.log(aliments); // ['chocolat', 'sucre', 'lait', 'fraise', 'banane', 'poire']
+  // console.log(aliments); // ['chocolat', 'sucre', 'lait', 'fraise', 'banane', 'poire']
 
 // Exemple 2 : découper une chaîne de caractères en plusieurs éléments
-let phrase = "Bonjour !";
-// Décomposer les caractères de la chaîne
-let phraseTableau = [...phrase];
+  let phrase = "Bonjour !";
+  // Décomposer les caractères de la chaîne
+  let phraseTableau = [...phrase];
 
-// console.log(phraseTableau); // ['B', 'o', 'n', 'j', 'o', 'u', 'r', ' ', '!']
+  // console.log(phraseTableau); // ['B', 'o', 'n', 'j', 'o', 'u', 'r', ' ', '!']
 
 // Exemple 3 : sélectionner un élément, et stocker les autres dans une variable
-// // Sans décomposition
-let devises = ["dollars", "euro", "yen"];
-let premiereDevise = devises[0];
-let autres = [devises[1], devises[2]];
+    // Sans décomposition
+  let devises = ["dollars", "euro", "yen"];
+  // let premiereDevise = devises[0];
+  // let autres = [devises[1], devises[2]];
 
-console.log(premiereDevise); // dollars
-console.log(autres); //['euro', 'yen']
+    // Avec décomposition
+  let [premiereDevise, ...autres] = devises;
+
+  console.log(premiereDevise); // dollars
+  console.log(autres); //['euro', 'yen']
