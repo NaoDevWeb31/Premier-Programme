a6704d05e17fee2a9a726d21e35348a894efb255
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 13:52:12 +0200

    [MAJ] Tableaux - Boucles foreach v1

--- exemple.js
+++ exemple.js
@@ -1,11 +1,3 @@
 //                  LES TABLEAUX
 
-let panier = ["fraise", "banane", "poire"];
-
-// Challenge - Récupérer l'index des éléments du tableau
-for (const fruit of panier) {
-  console.log(panier.indexOf(fruit));
-}
-// 0
-// 1
-// 2
+let listeDePays = ["France", "Belgique", "Japon", "Maroc"];
