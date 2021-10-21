7047e75442c5d852ff6ecfec24b489ab2f8a2559
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 11:43:45 +0200

    [MAJ] Tableaux - Accéder aux éléments des tableaux v2

--- exemple.js
+++ exemple.js
@@ -1,17 +1,17 @@
 //                  LES TABLEAUX
 
-let monTableau = new Array("un", "deux", "trois");
-let monTableau = Array("un", "deux", "trois");
+// let monTableau = new Array("un", "deux", "trois");
+// let monTableau = Array("un", "deux", "trois");
 let monTableau = ["un", "deux", "trois"]; // Désormais
 
 // Accéder à un élément du tableau
-console.log(monTableau[0]); // index 0 = élément 1
+console.log(monTableau[2]); // trois
 
 // Ancienne version - Tableau multidimensionnel
-let monTableau2D = new Array(
-  Array("Mark", "Jeff", "Bill"),
-  Array("Zuckerberg", "Bezos", "Gates")
-);
+// let monTableau2D = new Array(
+//   Array("Mark", "Jeff", "Bill"),
+//   Array("Zuckerberg", "Bezos", "Gates")
+// );
 
 // Nouvelle version - Tableau multidimensionnel
 let monTableau2D = [
@@ -19,9 +19,15 @@ let monTableau2D = [
   ["Zuckerberg", "Bezos", "Gates"],
 ];
 
+// Accéder à un élément d'un tableau multidimensionnel
+console.log(monTableau2D[0][2]); // Mark
+
 // Tableau associatif
 let monTableauAssociatif = {
   prenom: "Mark",
   nom: "Zuckerberg",
   poste: "PDG de Facebook",
 };
+
+// Accéder à un élément d'un tableau associatif
+console.log(monTableauAssociatif["poste"]); // PDG de Facebook
