1756a17283b6251ef97fbee8116a5c4d65cc9231
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 11:19:52 +0200

    [MAJ] Tableaux - Création d'un tableau simple

--- exemple.js
+++ exemple.js
@@ -1,15 +1,5 @@
-//                  LES FONCTIONS & LA LOGIQUE - Exercice
+//                  LES TABLEAUX
 
-function somme(nombre) { // 5
-  if (nombre == 1) {
-    return 1; // somme(1) = 1
-  }
-  return nombre + somme(nombre - 1); // 4
-  // return 4 + somme(4 - 1) => 3
-  // return 3 + somme(3 - 1) => 2
-  // return 2 + somme(2 - 1) => 1
-  // return 1
-
-  // 5 + 4 + 3 + 2 + 1
-}
-console.log(somme(5));
+let monTableau = new Array("un", "deux", "trois");
+let monTableau = Array("un", "deux", "trois");
+let monTableau = ["un", "deux", "trois"]; // Désormais
