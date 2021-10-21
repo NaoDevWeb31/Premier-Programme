76be40bb62f61c00ca625c6f92ac66443f0bbed1
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 11:30:13 +0200

    [MAJ] Tableaux - Tableau associatif

--- exemple.js
+++ exemple.js
@@ -4,14 +4,21 @@ let monTableau = new Array("un", "deux", "trois");
 let monTableau = Array("un", "deux", "trois");
 let monTableau = ["un", "deux", "trois"]; // Désormais
 
-// Ancienne version
+// Ancienne version - Tableau multidimensionnel
 let monTableau2D = new Array(
   Array("Mark", "Jeff", "Bill"),
   Array("Zuckerberg", "Bezos", "Gates")
 );
 
-// Nouvelle version
+// Nouvelle version - Tableau multidimensionnel
 let monTableau2D = [
   ["Mark", "Jeff", "Bill"],
   ["Zuckerberg", "Bezos", "Gates"],
 ];
+
+// Tableau associatif
+let monTableauAssociatif = {
+  prenom: "Mark",
+  nom: "Zuckerberg",
+  poste: "PDG de Facebook",
+};
