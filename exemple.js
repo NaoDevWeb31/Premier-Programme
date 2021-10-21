bd779b91cbce3fd2ee6b4ff0a1b6b6a0225dce30
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 15:34:43 +0200

    [MAJ] Objets - Map v1

--- exemple.js
+++ exemple.js
@@ -1,13 +1,5 @@
 //                  LES OBJETS
 
-let nombres = [10, 45, 75, 10, 24, 45];
+let monMap = new Map();
 
-// let monSet = new Set(nombres);
-let monSet = new Set(); // Créer un set
-
-monSet.add("70"); // Ajouter un string au set
-monSet.add(87); // Ajouter un number au set
-
-// monSet.delete(87); // Supprimer la valeur du set
-
-console.log(monSet.size); // Connaître le nombre d'éléments dans le set => 2
+console.log(monMap); // Map(0) {}
