e72927460978e4cea35cdb7a976f6a58bf9859d9
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 14:22:15 +0200

    [MAJ] Fonctions fléchées v2

--- exemple.js
+++ exemple.js
@@ -4,4 +4,17 @@ let maFonction = function() {
   console.log("test");
 };
 
+// Sans paramètre
+let maFonction = () => {
+
+}
+// Un paramètre
+let maFonction = parametre => {
+
+}
+// Plusieurs paramètre
+let maFonction = (parametre, autreParametre) => {
+
+}
+
 maFonction();
