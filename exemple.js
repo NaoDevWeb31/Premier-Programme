7d8ae35c5f71820098fdb7cd85df6680dbb48e38
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 12:09:43 +0200

    [MAJ] Tableaux - Suppression d'un élément v1

--- exemple.js
+++ exemple.js
@@ -2,17 +2,15 @@
 
 // Tableau simple
 let monTableau = ["un", "deux", "trois", "quatre"];
-monTableau.push("cinq"); // ["un", "deux", "trois", "quatre", "cinq"]
-monTableau.unshift("zero"); // ["zero", un", "deux", "trois", "quatre", "cinq"]
-// console.log(monTableau);
+monTableau.pop(); // Retirer un élément du tableau à la FIN
+monTableau.shift(); // Retirer un élément du tableau au DÉBUT
+console.log(monTableau);
 
 // Tableau dimensionnel
 let monTableau2D = [
   ["Mark", "Jeff", "Bill"],
   ["Zuckerberg", "Bezos", "Gates"],
 ];
-monTableau2D[0].push("test"); // ["Mark", "Jeff", "Bill", "test"]
-monTableau2D[1].unshift("test"); // ["test", Zuckerberg", "Bezos", "Gates"]
 // console.log(monTableau2D);
 
 // Tableau associatif
@@ -21,5 +19,4 @@ let monTableauAssociatif = {
   nom: "Zuckerberg",
   poste: "PDG de Facebook",
 };
-monTableauAssociatif["nationalité"] = "Américaine"; // {prenom: 'Mark', nom: 'Zuckerberg', poste: 'PDG de Facebook', nationalité: 'Américaine'}
-console.log(monTableauAssociatif);
+// console.log(monTableauAssociatif);
