8ec57c35e62c6ca471e85e49c6f0ac164101572d
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 14:44:39 +0200

    [MAJ] Closures v3

--- exemple.js
+++ exemple.js
@@ -4,21 +4,33 @@
 // // Sauvegarder les variables locales qu'elle utilise
 
 // Avec closure
-function bonjour(prenom) {
+// function bonjour(prenom) {
 
-  let resultat = "Bonjour " + prenom; // locale
-  let maClosure = () => console.log(resultat);
+//   let resultat = "Bonjour " + prenom; // locale
+//   let maClosure = () => console.log(resultat);
+//   return maClosure;
+
+// }
+// let maFonction = bonjour("Evan"); // stocke directement la variable "resultat"
+// maFonction(); // Bonjour Evan
+
+// // Sans closure
+// function bonjour_bis(prenom) {
+
+//   let resultat = "Bonjour " + prenom; // locale
+//   console.log(resultat);
+
+// }
+// bonjour_bis("Nicolas");
+
+function timer() {
+  let secondes = 0;
+
+  let maClosure = () => {
+    return ++secondes;
+  };
   return maClosure;
-
 }
-let maFonction = bonjour("Evan"); // stocke directement la variable "resultat"
-maFonction(); // Bonjour Evan
 
-// Sans closure
-function bonjour_bis(prenom) {
-
-  let resultat = "Bonjour " + prenom; // locale
-  console.log(resultat);
-
-}
-bonjour_bis("Nicolas");
+let monTimer = timer();
+console.log(monTimer()); // 1
