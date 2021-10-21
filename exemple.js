15866d6b3f6efb53a2345bb6bb6e6877f263c728
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 14:50:24 +0200

    [MAJ] Objets - Création v1

--- exemple.js
+++ exemple.js
@@ -1,43 +1,5 @@
-//                  LES CLOSURES (OU FERMETURES)
+//                  LES OBJETS
 
-// // Fonction dans une fonction
-// // Sauvegarder les variables locales qu'elle utilise
-
-// Avec closure
-// function bonjour(prenom) {
-
-//   let resultat = "Bonjour " + prenom; // locale
-//   let maClosure = () => console.log(resultat);
-//   return maClosure;
-
-// }
-// let maFonction = bonjour("Evan"); // stocke directement la variable "resultat"
-// maFonction(); // Bonjour Evan
-
-// // Sans closure
-// function bonjour_bis(prenom) {
-
-//   let resultat = "Bonjour " + prenom; // locale
-//   console.log(resultat);
-
-// }
-// bonjour_bis("Nicolas");
-
-function timer() {
-  let secondes = 0;
-
-  let maClosure = () => {
-    return ++secondes;
-  };
-  return maClosure;
-}
-
-let monTimer = timer();
-console.log(monTimer()); // 1
-console.log(monTimer()); // 2
-console.log(monTimer()); // 3
-
-let monDeuxiemeTimer = timer();
-console.log(monDeuxiemeTimer()); // 1
-
-console.log(monTimer()); // 4
+let chien = {
+  
+};
