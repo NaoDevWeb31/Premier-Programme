36268c2aa95ce005ca3a69cf95889d9ea15a4ac4
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 13:26:42 +0200

    [MAJ] Tableaux - Boucles for...in v1

--- exemple.js
+++ exemple.js
@@ -1,23 +1,10 @@
 //                  LES TABLEAUX
 
-// Tableau simple
-let monTableau = ["un", "deux", "trois", "quatre"];
-monTableau.splice(1, 0, "random", "pie"); // ['un', 'random', 'pie', 'deux', 'trois', 'quatre']
-// console.log(monTableau);
+let panier = ["fraise", "banane", "poire"];
 
-// Tableau dimensionnel
-let monTableau2D = [
-  ["Mark", "Jeff", "Bill"],
-  ["Zuckerberg", "Bezos", "Gates"],
-];
-// // CHALLENGE
-monTableau2D.splice(2, 0, ["30", "45", "70"]); // [ ["Mark", Jeff", "Bill"], ["Zuckerberg", "Bezos", "Gates"], ["30", "45", "70"] ]
-console.log(monTableau2D);
-
-// Tableau associatif
-let monTableauAssociatif = {
-  prenom: "Mark",
-  nom: "Zuckerberg",
-  poste: "PDG de Facebook",
-};
-// console.log(monTableauAssociatif);
+for (const fruit in panier) {
+  console.log(fruit);
+}
+// 0
+// 1
+// 2
