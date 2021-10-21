5b0bdec634bfce49d1a8832df80ef14b69d737c7
Naomi Mango <naomi.mango@gmail.com>
2021-10-21 11:26:50 +0200

    [MAJ] Tableaux - Tableau multidimensionnel

--- exemple.js
+++ exemple.js
@@ -3,3 +3,15 @@
 let monTableau = new Array("un", "deux", "trois");
 let monTableau = Array("un", "deux", "trois");
 let monTableau = ["un", "deux", "trois"]; // Désormais
+
+// Ancienne version
+let monTableau2D = new Array(
+  Array("Mark", "Jeff", "Bill"),
+  Array("Zuckerberg", "Bezos", "Gates")
+);
+
+// Nouvelle version
+let monTableau2D = [
+  ["Mark", "Jeff", "Bill"],
+  ["Zuckerberg", "Bezos", "Gates"],
+];
