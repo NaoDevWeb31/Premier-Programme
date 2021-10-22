7052f972d4ac1eec09f88250e835acc2f4103b76
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 13:43:22 +0200

    [MAJ] DOM - Modifier des éléments v1

--- exemple.js
+++ exemple.js
@@ -1,14 +1,9 @@
 //                              Document Object Model (DOM)
-/****** Accéder aux éléments du DOM ******/
+/****** Modifier des éléments du DOM ******/
 
-let header = document.getElementsByTagName("header"); // Récupérer un tableau de balise(s)
-let logo = document.getElementById("logo"); // Récupérer un id
-let container = document.getElementsByClassName("container"); // Récupérer un tableau de classe(s)
-let h1 = document.querySelector("h1"); // Récupérer le premier sélecteur correspondant
-let p = document.querySelectorAll("p"); // Récupérer un tableau de tous les sélecteurs correspondants
+let h1 = document.querySelector("h1");
 
-console.log(header); // HTMLCollection [header]
-console.log(logo); // div#logo
-console.log(container); // HTMLCollection [div.container]
-console.log(h1); // h1
-console.log(p); // NodeList(2) [p, p]
+// Modifier le contenu textuel grâce à la propriété "textContent"
+h1.textContent = "Hello World";
+
+console.log(h1);
