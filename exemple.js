abfc683b3a4e3c29f4ec3c7a1c93e33ffc34a260
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 12:43:09 +0200

    [MAJ] DOM - Accéder aux éléments v1

--- exemple.js
+++ exemple.js
@@ -1,27 +1,2 @@
-//                  Document Object Model (DOM)
-
-• Tous les éléments pouvant être utilisés par l'objet Document, accessibles depuis JavaScript
-  •• Toute la structure d'une page HTML (balise, classe, id, contenu)
-
-
-=> Mettez votre balise <script></script> juste avant la fin de votre </body>
-  • Donner le temps au navigateur de lire l'ensemble de notre page
-  • Il saura parfaitement
-    •• s'il y a des titres
-    •• combien il y a de liens
-    •• etc...
-  • Il peut donc trouver nos éléments sans problème grâce à JavaScript
-
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
-        ...
-        ...
-        ...
-        ...
-    // ICI //
-        <script src="exemple.js"></script>
-    </body>
-</html>
+//                              Document Object Model (DOM)
+/****** Accéder aux éléments du DOM ******/
