379d49f1e59f18ef3c4f75c98b8c6029c15a1549
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 13:31:05 +0200

    [MAJ] DOM - Accéder aux éléments v4

--- exemple.js
+++ exemple.js
@@ -3,6 +3,8 @@
 
 let header = document.getElementsByTagName("header"); // Récupérer un tableau de balise(s)
 let logo = document.getElementById("logo"); // Récupérer un id
+let container = document.getElementsByClassName("container"); // Récupérer un tableau de classe(s)
 
 console.log(header); // HTMLCollection [header]
 console.log(logo); // div#logo
+console.log(container); // HTMLCollection [div.container]
