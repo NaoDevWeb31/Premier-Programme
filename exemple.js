c92e8796c82ce37a57c1aeab057365a17e58b94b
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 13:29:10 +0200

    [MAJ] DOM - Accéder aux éléments v2

--- exemple.js
+++ exemple.js
@@ -1,2 +1,6 @@
 //                              Document Object Model (DOM)
 /****** Accéder aux éléments du DOM ******/
+
+let header = document.getElementsByTagName("header"); // Récupérer un tableau de balise(s)
+
+console.log(header); // HTMLCollection [header]
