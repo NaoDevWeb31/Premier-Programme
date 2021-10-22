7603021340e955fa76b5983fad8be3da3b51686a
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 13:54:08 +0200

    [MAJ] DOM - Ajouter des éléments v3

--- exemple.js
+++ exemple.js
@@ -6,3 +6,5 @@ document.write("test"); // "test" écrit à la suite du contenu textuel
 
 // 2e méthode : Ajouter un élément brut
 document.body.append("test"); // à la suite du contenu textuel
+let h1 = document.querySelector("h1");
+h1.append("test"); // Bienvenue sur mon site !test
