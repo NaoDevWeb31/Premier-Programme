6dc2ae8c87e04087a7c8199674b8e1dee4ee2448
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 15:38:44 +0200

    [MAJ] Projet 2 v5

--- Projet_2-DOM/code.js
+++ Projet_2-DOM/code.js
@@ -4,10 +4,12 @@ document.querySelector("#a-supprimer").remove();
 // Créer un header
 let header = document.createElement("header");
 document.body.insertBefore(header, document.querySelector("script"));
+header.style.backgroundColor = "#e3b04b";
 
 // Créer un sous-header
 let sousHeader = document.createElement("header");
 document.body.insertBefore(sousHeader, document.querySelector("script"));
+sousHeader.style.backgroundColor = "#f1d6ab";
 
 // Créer un paragraphe
 let p = document.createElement("p");
