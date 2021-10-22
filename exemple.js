8c4c94be5a0a9bc004e60c589d968c3c01185dce
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 12:15:23 +0200

    [MAJ] BOM - Objet Screen

--- exemple.js
+++ exemple.js
@@ -1,11 +1,15 @@
 //                  BROWSER OBJECT MODEL (BOM)
-// Objet "Location" //
-• Déterminer l'emplacement (web) de l'utilisateur
-• Avoir une action sur l'URL de l'utilisateur
-• Donner des informations relatives aux adresses
+// Objet "Screen" //
+• Donner des informations sur l'écran de l'utilisateur
+  •• palette de couleurs utilisée
+  •• taille de l'écran
+  •• résolution
 
-// Recharger une page
-location .reload();
+// Largeur de l'écran
+screen.availWidth; // 1280px
 
-// Rediriger
-location.href();
+// Hauteur de l'écran
+screen.availHeight; // 777px
+
+// Résolution
+screen.pixelDepth; // 24
