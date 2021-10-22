e583423b88f50265d00d97f5231d6811b22a9ea7
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 17:55:02 +0200

    [MAJ] Évènements - Exercice 1 - Étape 4

--- exemple.js
+++ exemple.js
@@ -5,4 +5,10 @@
 let btn = document.querySelector("button");
 
 // Étape 3 - Ajoutez un évènement sur votre bouton, qui se déclenchera au clic, et qui appellera la fonction start()
-btn.addEventListener("click", start());
+btn.addEventListener("click", start);
+
+// Étape 4 - Créez une fonction start() qui aura pour but de créer une variable stockant un intervalle. Cet intervalle devra appeler une fonction decompte() toutes les secondes
+/// Je vous conseille d’initialiser toutes vos variables à l’extérieur de vos fonctions pour pouvoir les utiliser partout (hors, évidemment, la variable qui contient notre intervalle)
+function start() {
+  var interval = setInterval(decompte, 1000);
+}
