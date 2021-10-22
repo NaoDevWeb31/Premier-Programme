23a296c9a88b9076881f4b89c751c33305403695
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 13:43:32 +0200

    [MAJ] DOM - Modifier des éléments v2

--- exemple.js
+++ exemple.js
@@ -6,4 +6,7 @@ let h1 = document.querySelector("h1");
 // Modifier le contenu textuel grâce à la propriété "textContent"
 h1.textContent = "Hello World";
 
+// Modifier le contenu HTML grâce à la propriété "innerHTML"
+h1.innerHTML = `<div style="font-weight: normal"> Hello World !</div>`;
+
 console.log(h1);
