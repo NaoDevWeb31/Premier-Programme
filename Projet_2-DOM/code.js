6146cc88a598735ac7aea7a67524ad9eecf10dcf
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 15:33:07 +0200

    [MAJ] Projet 2 v3

--- Projet_2-DOM/code.js
+++ Projet_2-DOM/code.js
@@ -4,3 +4,7 @@ document.querySelector("#a-supprimer").remove();
 // Créer un header
 let header = document.createElement("header");
 document.body.insertBefore(header, document.querySelector("script"));
+
+// Créer un sous-header
+let sousHeader = document.createElement("header");
+document.body.insertBefore(sousHeader, document.querySelector("script"));
