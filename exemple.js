8c93b14fb22a070df1c22850d693c5d2ec86c280
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 18:17:49 +0200

    [MAJ] Évènements - Exercice 1 - Étape 5

--- exemple.js
+++ exemple.js
@@ -9,6 +9,24 @@ btn.addEventListener("click", start);
 
 // Étape 4 - Créez une fonction start() qui aura pour but de créer une variable stockant un intervalle. Cet intervalle devra appeler une fonction decompte() toutes les secondes
 /// Je vous conseille d’initialiser toutes vos variables à l’extérieur de vos fonctions pour pouvoir les utiliser partout (hors, évidemment, la variable qui contient notre intervalle)
+let interval;
+
 function start() {
-  var interval = setInterval(decompte, 1000);
+  interval = setInterval(decompte, 1000);
+}
+
+// Étape 5 - Créez une fonction decompte() qui décrémentera dans un premier temps la variable secondes
+/// Créez cette variable en-dehors de votre fonction
+/// Puis, vérifiez si secondes vaut 0
+/// Si c’est le cas, appelez la fonction stop()
+/// sinon, affichez dans la page HTML, grâce à JavaScript, le nombre de secondes restantes (par exemple, s’il reste 5 secondes, écrivez « 5 » dans la page, à la suite des autres secondes, comme dans l’exemple ci-dessus)
+let secondes = 5;
+
+function decompte() {
+  secondes--;
+  if (secondes == 0) {
+    stop();
+  } else {
+    document.body.innerHTML += `<div style="text-align:center; font-size:1.5rem; margin: 0rem 0em 1rem 0em;">${secondes}</div>`;
+  }
 }
