6872eebdc652ba5471e19848e064d7f824faac2a
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 12:31:07 +0200

    [MAJ] DOM - Définition

--- exemple.js
+++ exemple.js
@@ -1,12 +1,27 @@
-//                  BROWSER OBJECT MODEL (BOM)
-// Objet "Document" //
-• Agir sur le document (site internet) de l'utilisateur
-• Avoir des actions sur le HTML du site internet
-  •• ajouter des éléments
-  •• modifier des éléments
-  •• supprimer des éléments
-  •• écouter des évènements
-  •• etc.
-• Objet le plus sollicité avec JavaScript
-• Permet d'avoir des interactions entre JavaScript et le site internet
-• Permet de faire des effets grâce à JavaScript
+//                  Document Object Model (DOM)
+
+• Tous les éléments pouvant être utilisés par l'objet Document, accessibles depuis JavaScript
+  •• Toute la structure d'une page HTML (balise, classe, id, contenu)
+
+
+=> Mettez votre balise <script></script> juste avant la fin de votre </body>
+  • Donner le temps au navigateur de lire l'ensemble de notre page
+  • Il saura parfaitement
+    •• s'il y a des titres
+    •• combien il y a de liens
+    •• etc...
+  • Il peut donc trouver nos éléments sans problème grâce à JavaScript
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        ...
+        ...
+        ...
+        ...
+    // ICI //
+        <script src="exemple.js"></script>
+    </body>
+</html>
