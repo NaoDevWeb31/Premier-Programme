57c1a6e515914c4876135887f83c73b3c65da611
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 17:10:30 +0200

    [MAJ] Évènements - Les écouteurs - Challenge 3

--- exemple.js
+++ exemple.js
@@ -5,6 +5,18 @@ let a = document.querySelector("a");
 let button = document.querySelector("button");
 
 // Au clic du lien
-a.addEventListener("click", (e) => {
-  console.log(e); // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, type: "click", …}
+a.addEventListener("click", () => {
+  if (confirm("Êtes-vous sûr ?")) {
+    location.href = "https://believemy.com";
+  }
+});
+
+// Challenge //
+// Au survol du bouton
+button.addEventListener("mouseover", () => {
+  document.body.style.backgroundColor = "orange";
+});
+// Souris en dehors du bouton
+button.addEventListener("mouseout", () => {
+  document.body.style.backgroundColor = "";
 });
