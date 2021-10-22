649f6202bd74b1969f59ebf13084867faa539746
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 13:53:02 +0200

    [MAJ] DOM - Ajouter des éléments v2

--- exemple.js
+++ exemple.js
@@ -3,3 +3,6 @@
 
 // 1ère méthode : Juste écrire un texte
 document.write("test"); // "test" écrit à la suite du contenu textuel
+
+// 2e méthode : Ajouter un élément brut
+document.body.append("test"); // à la suite du contenu textuel
