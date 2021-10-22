c8f3a2d050404b92e2c8c6beadea231b68af0838
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 17:48:32 +0200

    [MAJ] Évènements - Exercice 1 - Étape 3

--- exemple.js
+++ exemple.js
@@ -3,3 +3,6 @@
 
 // Étape 2 - Récupérez grâce à JavaScript, votre élément button, et stockez-le dans une variable btn
 let btn = document.querySelector("button");
+
+// Étape 3 - Ajoutez un évènement sur votre bouton, qui se déclenchera au clic, et qui appellera la fonction start()
+btn.addEventListener("click", start());
