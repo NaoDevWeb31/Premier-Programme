541002fcc96674187596c299a9c16777767a6f6f
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 17:32:26 +0200

    [MAJ] Évènements - Planifier l'exécution d'un script v3

--- exemple.js
+++ exemple.js
@@ -2,7 +2,8 @@
 /****** Planifier l'exécution d'un script ******/
 
 // setTimeout
-// setTimeout("alert('Bonjour')", 3000); // Popup "Bonjour" s'affiche au bout de 3s
+let timer = setTimeout("alert('Bonjour')", 3000); // Popup "Bonjour" s'affiche au bout de 3s
+clearTimeout(timer); // Arrêter le timer
 
 // setInterval
-setInterval("alert('Bonjour')", 5000); // Popup "Bonjour" s'affiche toutes les 5s après sa fermeture
+// setInterval("alert('Bonjour')", 5000); // Popup "Bonjour" s'affiche toutes les 5s après sa fermeture
