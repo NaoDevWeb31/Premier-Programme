9625b6ec8cf4317972aaf421a614ac41fddfda1d
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 12:07:49 +0200

    [MAJ] BOM - Objet History

--- exemple.js
+++ exemple.js
@@ -1,19 +1,10 @@
 //                  BROWSER OBJECT MODEL (BOM)
-// Objet "Navigator" //
-• Fournir tout un tas d'informations sur le navigateur de nos utilisateurs
-• Pour certaines, demander une permission grâce à une boîte de dialogue
-• Récupérer la position géographique de l'utilisateur
-• Permet d'utiliser certaines fonctionnalités du navigateur
-  •• géolocalisation
-  •• langue de l'utilisateur
-  •• etc.
-• ex: Afficher la date dans le bon format en récupérant la langue de l'utilisateur
+// Objet "History" //
+• Avoir une action sur l'historique de nos utilisateurs
+• Sauf dans le cas de la navigation privée
 
-// Les cookies sont-ils autorisés ?
-navigator.cookieEnabled; // true
+// Page précédente
+history.back();
 
-// Système d'exploitation ?
-navigator.platform; // MacIntel
-
-// Langue du navigateur ?
-navigator.language;  fr-FR
+// Page suivante (si existante)
+history.forward();
