76a2ef74c5f98f17bfa73de121422615ba8a9da3
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 15:30:30 +0200

    [MAJ] Projet 2 v2

--- Projet_2-DOM/code.js
+++ Projet_2-DOM/code.js
@@ -1,2 +1,6 @@
 // Supprimer le div avec l'id #a-supprimer
 document.querySelector("#a-supprimer").remove();
+
+// Créer un header
+let header = document.createElement("header");
+document.body.insertBefore(header, document.querySelector("script"));
