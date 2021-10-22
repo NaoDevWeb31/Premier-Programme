cd54cd03d95859558886591ffc042a0a45dc0c40
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 14:12:25 +0200

    [MAJ] DOM - Supprimer des éléments v1

--- exemple.js
+++ exemple.js
@@ -1,29 +1,5 @@
 //                              Document Object Model (DOM)
-/****** Ajouter des éléments au DOM ******/
+/****** Supprimer des éléments du DOM ******/
 
-// 1ère méthode : Juste écrire un texte
-document.write("test"); // "test" écrit à la suite du contenu textuel
-
-// 2e méthode : Ajouter un élément brut
 let h1 = document.querySelector("h1");
-h1.append("test"); // Bienvenue sur mon site !test
-
-// 3e méthode : pour les objets
-  // Créer l'élément (balise)
-  let helloWorld = document.createElement("div");
-
-  // Personnaliser l'élément - Challenge
-  helloWorld.textContent = "Hello World !";
-
-  // Ajouter l'élément à la page
-    // #1 - après
-    // document.body.append(helloWorld);
-
-    // #2 - après
-    // document.body.appendChild(helloWorld); // Préférer append à appendChild => ne peut pas passer de texte
-    
-    // #3 - avant
-    // document.body.insertBefore(helloWorld, document.querySelector(".container")); // (élément à insérer, emplacement)
-    
-    // #4 - avant
-    document.querySelector(".container").prepend(helloWorld);
+h1.remove();
