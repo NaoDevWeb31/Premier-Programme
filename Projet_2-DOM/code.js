720bb43564c565b0716919d96a276497701dd808
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 16:10:18 +0200

    [MAJ] Projet 2 v11

--- Projet_2-DOM/code.js
+++ Projet_2-DOM/code.js
@@ -16,13 +16,14 @@ let sousHeader = document.createElement("header");
 document.body.insertBefore(sousHeader, document.querySelector("script"));
 sousHeader.style.backgroundColor = "#f1d6ab";
 sousHeader.style.margin = "0rem -0.5rem";
-sousHeader.style.padding = "2rem";
+sousHeader.style.padding = "1rem";
 sousHeader.innerHTML = `<a href='#'>Accueil</a> / <a href='#'>Une autre page</a>`;
 
 let links = sousHeader.querySelectorAll("a");
-links.forEach(link => link.style.color = "#167BFF");
+links.forEach((link) => (link.style.color = "#167BFF"));
 
 // Créer un paragraphe
 let p = document.createElement("p");
 document.body.insertBefore(p, document.querySelector("script"));
 p.textContent = "Ceci est un paragraphe crée avec JavaScript !";
+p.style.margin = "1rem 0rem";
