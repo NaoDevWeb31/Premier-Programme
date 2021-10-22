c60639fc0d6bb05d8bf10c3674247d1abf1e4947
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 17:01:36 +0200

    [MAJ] Évènements - Les écouteurs v7

--- exemple.js
+++ exemple.js
@@ -1,28 +1,7 @@
 //                              LES ÉVÈNEMENTS
-/****** Les écouteurs via les propriétés JavaScript ******/
+/****** Les écouteurs via le gestionnaire d'évènements ******/
 
 let a = document.querySelector("a");
 let button = document.querySelector("button");
 
-// Au clic du lien
-// a.onclick = function () {
-//   if (confirm("Êtes-vous sûr ?")) {
-//     location.href = "https://believemy.com";
-//   }
-// };
-
-// Challenge
-a.onclick = () => {
-  if (confirm("Êtes-vous sûr ?")) {
-    location.href = "https://believemy.com";
-  }
-};
-
-// Au survol de la souris
-// Challenge
-button.onmouseover = () => {
-  document.body.style.backgroundColor = "orange";
-};
-button.onmouseout = () => {
-  document.body.style.backgroundColor = "";
-};
+a.addEventListener("click")
