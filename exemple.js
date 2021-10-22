51d9324b58124b2eee7d31a1265fddb7976d9f94
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 17:46:51 +0200

    [MAJ] Évènements - Exercice 1 - Étape 2

--- exemple.js
+++ exemple.js
@@ -1,2 +1,5 @@
 //                              LES ÉVÈNEMENTS - EXERCICE
 /****** Faire un timer ******/
+
+// Étape 2 - Récupérez grâce à JavaScript, votre élément button, et stockez-le dans une variable btn
+let btn = document.querySelector("button");
