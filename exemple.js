c369640b2604d46d7052bc2647601736b3d0a207
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 16:50:09 +0200

    [MAJ] Évènements - Les écouteurs v5

--- exemple.js
+++ exemple.js
@@ -3,3 +3,10 @@
 
 let a = document.querySelector("a");
 let button = document.querySelector("button");
+
+// Au clic du lien
+a.onclick = function () {
+  if (confirm("Êtes-vous sûr ?")) {
+    location.href = "https://believemy.com";
+  }
+};
