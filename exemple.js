f745d1a585f612a8e7a1e3614bf45b74f706356e
Naomi Mango <naomi.mango@gmail.com>
2021-10-22 13:52:01 +0200

    [MAJ] DOM - Ajouter des éléments v1

--- exemple.js
+++ exemple.js
@@ -1,17 +1,5 @@
 //                              Document Object Model (DOM)
-/****** Modifier des éléments du DOM ******/
+/****** Ajouter des éléments au DOM ******/
 
-let h1 = document.getElementsByTagName("h1");
-
-console.log(h1); // HTMLCollection [h1, textContent: 'Hello World', innerHTML: '<div style="font-weight: normal"> Hello World !</div>']
-
-// Récupérer le premier élément du tableau HTMLCollection avant de modifier
-h1 = h1[0];
-
-// Modifier le contenu textuel grâce à la propriété "textContent"
-h1.textContent = "Hello World";
-
-// Modifier le contenu HTML grâce à la propriété "innerHTML"
-h1.innerHTML = `<div style="font-weight: normal"> Hello World !</div>`;
-
-console.log(h1);
+// 1ère méthode : Juste écrire un texte
+document.write("test"); // "test" écrit à la suite du contenu textuel
