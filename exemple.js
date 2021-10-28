03398c178910bc03a7bdbd6ec32d7a167f37edfb
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 19:33:25 +0200

    [MAJ] Programmation orientée objet - Classe & constructeur v1

--- exemple.js
+++ exemple.js
@@ -1,37 +1,17 @@
 //                              LA PROGRAMMATION ORIENTÉE OBJET
-/****** Bind, Call & Apply ******/
+/****** Créer un objet avec une classe et un constructeur ******/
 
-// Call et Apply permettent d'invoquer immédiatement une fonction
-// Call
-// Animal.call(this, nombreDePattes, poids)
+class Utilisateur {
+  constructor(prenom, nom, email) {
+    this.prenom = prenom;
+    this.nom = nom;
+    this.email = email;
+  }
 
-let gandalf = { titre: "Magicien" };
-
-function direBonjour(arme, degats) {
-  console.log("Bonjour " + this.titre + ", vous possédez " + arme + " qui donne " + degats + " points de dégâts.");
-}
-
-// Appeler l'objet gandalf pour le transmettre à direBonjour()
-direBonjour.apply(gandalf, ["un bâton", 75]); // passer les arguments via un tableau (pour de gros fichiers JSON)
-direBonjour.call(gandalf, "un bâton", 75); // passer les arguments un à un (pour passer certaines variables)
-
-// Bind
-// Objectif : Changer le contexte de "this"
-
-this.valeur = "Window";
-
-let monObjet = {
-  valeur: "Objet",
-  getValeur: function () {
-    console.log(this.valeur);
+  sePresenter(){
+    console.log("Je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email + ".");
   }
 }
 
-monObjet.getValeur(); // Objet
-
-let maValeur = monObjet.getValeur;
-maValeur(); // Window, car console.log(this.valeur) fait référence à l'objet global
-
-// Pour corriger ce problème, on utilise bind(), qui va récréer une fonction avec le contexte de l'objet monObjet
-let maValeurAvecBind = monObjet.getValeur.bind(monObjet); // ici, on "prend" le contexte de base
-maValeurAvecBind(); // Objet
+var mark = new Utilisateur("Mark", "Zuckerberg", "mark@facebook.com");
+mark.sePresenter();
