10e001a51d3146cfc1edccfafb68b2a871472756
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 18:25:18 +0200

    [MAJ] Programmation orientée objet - Comprendre l'héritage v1

--- exemple.js
+++ exemple.js
@@ -1,34 +1,18 @@
 //                              LA PROGRAMMATION ORIENTÉE OBJET
-/****** Créer un objet avec le constructeur Object ******/
+/****** Comprendre l'héritage ******/
 
-// Propriétés spécifiques à l'objet
-function Utilisateur(prenom, nom, email) {
-  this.prenom = prenom;
-  this.nom = nom;
-  this.email = email;
-}
-// Méthodes générales
-Utilisateur.prototype.sePresenter = function() {
-  console.log("Bonjour, je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email);
+// Animal(nombreDePattes, poids)
+function Animal(nombreDePattes, poids) {
+  this.nombreDePattes = nombreDePattes;
+  this.poids = poids;
 }
 
-// On crée un objet
-var mark = new Utilisateur("Mark", "Zuckerberg", "mark@facebook.com");
-var bill = new Utilisateur("Bill", "Gates", "bill@gatenotes.com");
+// Oiseau(nombreDePattes, poids / longueurDesAiles)
+function Oiseau(longueurDesAiles) {
+  this.longueurDesAiles = longueurDesAiles;
+}
 
-console.log(mark); // On voit que JavaScript ajoute une propriété __proto__ et constructor
-/*
-Utilisateur
-  email: "mark@facebook.com"
-  nom: "Zuckerberg"
-  prenom: "Mark"
-  [[Prototype]]: Object
-    sePresenter: ƒ ()
-    constructor: ƒ Utilisateur(prenom, nom, email)
-    [[Prototype]]: Object 
-*/
-bill.sePresenter(); // Bonjour, je m'appelle Bill Gates et vous pouvez me contacter à l'adresse email bill@gatenotes.com
-
-// Créer un objet avec Object()
-var monObjet = new Object();
-monObjet.titre = "Le titre de l'objet";
+// Mammifere(nombreDePattes, poids / typeDePoils)
+function Mammifere(typeDePoils) {
+  this.typeDePoils = typeDePoils;
+}
