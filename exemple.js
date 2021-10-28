22fa1103e43f94a87933b87737e735f98043191e
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 19:47:27 +0200

    [MAJ] Programmation orientée objet - Classe & constructeur v2

--- exemple.js
+++ exemple.js
@@ -1,17 +1,44 @@
 //                              LA PROGRAMMATION ORIENTÉE OBJET
 /****** Créer un objet avec une classe et un constructeur ******/
 
-class Utilisateur {
-  constructor(prenom, nom, email) {
-    this.prenom = prenom;
-    this.nom = nom;
-    this.email = email;
+// class Utilisateur {
+//   constructor(prenom, nom, email) {
+//     this.prenom = prenom;
+//     this.nom = nom;
+//     this.email = email;
+//   }
+
+//   sePresenter(){
+//     console.log("Je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email + ".");
+//   }
+// }
+
+// var mark = new Utilisateur("Mark", "Zuckerberg", "mark@facebook.com");
+// mark.sePresenter();
+
+class Animal {
+  constructor(nombreDePattes, poids){
+    this.nombreDePattes = nombreDePattes;
+    this.poids = poids;
   }
 
-  sePresenter(){
-    console.log("Je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email + ".");
+  presentation(){
+    console.log("Cet animal possède " + this.nombreDePattes + " pattes et fait " + this.poids + ".");
   }
 }
 
-var mark = new Utilisateur("Mark", "Zuckerberg", "mark@facebook.com");
-mark.sePresenter();
+class Oiseau extends Animal{ // Oiseau.prototype = Object.create(Animal.prototype)
+  constructor(nombreDePattes, poids, longueurDesAiles){
+    super(nombreDePattes, poids);
+    this.longueurDesAiles = longueurDesAiles;
+  }
+
+  voler(){
+    console.log("L'oiseau vole !");
+  }
+}
+
+var perroquet = new Oiseau(2, "1kg", "grandes");
+console.log(perroquet);
+perroquet.presentation();
+perroquet.voler();
