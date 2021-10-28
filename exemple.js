79de18802bea8983f7f8e27f1b205dda4fcd6794
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 17:24:41 +0200

    [MAJ] Programmation orientée objet - Objet par construteur personnalisé v1

--- exemple.js
+++ exemple.js
@@ -1,26 +1,22 @@
 //                              LA PROGRAMMATION ORIENTÉE OBJET
-/****** Créer un objet littéral ******/
+/****** Créer un objet avec un constructeur personnalisé ******/
 
-/// Généralement pas utilisé de cette façon
-let mark = {
-  prenom: "Mark", // propriété
-  nom: "Zuckerberg",
-  email: "mark@facebook.com",
+// let mark = {
+//   prenom: "Mark", // propriété
+//   nom: "Zuckerberg",
+//   email: "mark@facebook.com",
 
-  // Ajouter une méthode : fonction liée à un objet (ou à une classe)
-  /// À l'ancienne
-  sePresenter: function () {
-    console.log("Bonjour, je m'appelle " + this.prenom);
-  },
-};
+//   // Ajouter une méthode : fonction liée à un objet (ou à une classe)
+//   /// À l'ancienne
+//   sePresenter: function () {
+//     console.log("Bonjour, je m'appelle " + mark.prenom);
+//   },
+// };
 
-mark.sePresenter();
-
-/// Utilisé pour transmettre des informations
-function recevoirLesCoordonees() {
-  return { latitude: 35, longitude: 139 };
+// On crée une fonction constructeur pour nos utilisateurs
+// convention : constructeur avec la 1ère lettre en majuscule
+function Utilisateur(prenom, nom, email) {
+  this.prenom = prenom; // l'objet en cours = l'objet créé à partir du constructeur
+  this.nom = nom; // mark.prenom = prenom
+  this.email = email;
 }
-
-let coordonnes = recevoirLesCoordonees();
-console.log(coordonnes.latitude);
-console.log(coordonnes.longitude);
