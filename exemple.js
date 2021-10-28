51edebce27cd73a9f1d9be0cfcffdc2f8ec58722
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 19:11:03 +0200

    [MAJ] Programmation orientée objet - Bind, Call & Apply v2

--- exemple.js
+++ exemple.js
@@ -7,8 +7,10 @@
 
 let gandalf = { titre: "Magicien" };
 
-function direBonjour() {
-  console.log("Bonjour " + this.titre);
+function direBonjour(arme, degats) {
+  console.log("Bonjour " + this.titre + ", vous possédez " + arme + " qui donne " + degats + " points de dégâts.");
 }
+
 // Appeler l'objet gandalf pour le transmettre à direBonjour()
-direBonjour.call(gandalf);
+direBonjour.apply(gandalf, ["un bâton", 75]); // passer les arguments via un tableau (pour de gros fichiers JSON)
+direBonjour.call(gandalf, "un bâton", 75); // passer les arguments un à un (pour passer certaines variables)
