c279083993a52d37aaff17035d09614c898326d7
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 18:50:27 +0200

    [MAJ] Programmation orientée objet - Comprendre l'héritage v5

--- exemple.js
+++ exemple.js
@@ -13,11 +13,15 @@ Animal.prototype.presentation = function () {
 // Oiseau(nombreDePattes, poids / longueurDesAiles)
 function Oiseau(nombreDePattes, poids, longueurDesAiles) {
   // La fonction Call() permet d'appeler une fonction constructeur
+  // Faire hériter le constructeur Animal au constructeur Oiseau
   Animal.call(this, nombreDePattes, poids);
   this.longueurDesAiles = longueurDesAiles;
 }
 // Faire hériter le prototype du constructeur Animal au constructeur Oiseau
+// Utiliser le prototype de notre constructeur hérité
 Oiseau.prototype = Object.create(Animal.prototype);
+// Rajouter la propriété constructor supprimée à la ligne du dessus
+Oiseau.prototype.constructor = Oiseau;
 
 // Mammifere(nombreDePattes, poids / typeDePoils)
 function Mammifere(nombreDePattes, poids, typeDePoils) {
@@ -26,5 +30,4 @@ function Mammifere(nombreDePattes, poids, typeDePoils) {
 }
 
 var perroquet = new Oiseau(2, "1kg", "grandes");
-perroquet.presentation();
 console.log(perroquet);
