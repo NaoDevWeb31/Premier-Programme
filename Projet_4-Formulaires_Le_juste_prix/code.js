81eae3393c1c2d32d3f1c4248b6e26256090553d
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 14:58:05 +0200

    [NEW] Projet 4 - Étape 0

---
+++ Projet_4-Formulaires_Le_juste_prix/code.js
@@ -0,0 +1,11 @@
+// Étape 1 - Sélectionner nos éléments
+
+// Étape 2 - Cacher l'erreur
+
+// Étape 3 - Générer un nombre aléatoire
+
+// Étape 4 - Vérifier que l'utilisateur donne bien un nombre
+
+// Étape 5 - Agir à l'envoi du formulaire
+
+// Étape 6 - Créer la fonction vérifier
