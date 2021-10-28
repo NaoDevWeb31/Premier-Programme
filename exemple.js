56c8789c94d7b950631ac76834db4e049b9cf65a
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 19:23:14 +0200

    [MAJ] Programmation orientée objet - Bind, Call & Apply v3

--- exemple.js
+++ exemple.js
@@ -14,3 +14,24 @@ function direBonjour(arme, degats) {
 // Appeler l'objet gandalf pour le transmettre à direBonjour()
 direBonjour.apply(gandalf, ["un bâton", 75]); // passer les arguments via un tableau (pour de gros fichiers JSON)
 direBonjour.call(gandalf, "un bâton", 75); // passer les arguments un à un (pour passer certaines variables)
+
+// Bind
+// Objectif : Changer le contexte de "this"
+
+this.valeur = "Window";
+
+let monObjet = {
+  valeur: "Objet",
+  getValeur: function () {
+    console.log(this.valeur);
+  }
+}
+
+monObjet.getValeur(); // Objet
+
+let maValeur = monObjet.getValeur;
+maValeur(); // Window, car console.log(this.valeur) fait référence à l'objet global
+
+// Pour corriger ce problème, on utilise bind(), qui va récréer une fonction avec le contexte de l'objet monObjet
+let maValeurAvecBind = monObjet.getValeur.bind(monObjet); // ici, on "prend" le contexte de base
+maValeurAvecBind(); // Objet
