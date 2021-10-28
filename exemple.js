fda358d87574d97441999377b8f5dcc20a596175
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 19:01:59 +0200

    [MAJ] Programmation orientée objet - Bind, Call & Apply v1

--- exemple.js
+++ exemple.js
@@ -1,33 +1,14 @@
 //                              LA PROGRAMMATION ORIENTÉE OBJET
-/****** Comprendre l'héritage ******/
+/****** Bind, Call & Apply ******/
 
-// Animal(nombreDePattes, poids)
-function Animal(nombreDePattes, poids) {
-  this.nombreDePattes = nombreDePattes;
-  this.poids = poids;
-}
-Animal.prototype.presentation = function () {
-  console.log("Cet animal possède " + this.nombreDePattes + " pattes et fait " + this.poids + ".");
-}
+// Call et Apply permettent d'invoquer immédiatement une fonction
+// Call
+// Animal.call(this, nombreDePattes, poids)
 
-// Oiseau(nombreDePattes, poids / longueurDesAiles)
-function Oiseau(nombreDePattes, poids, longueurDesAiles) {
-  // La fonction Call() permet d'appeler une fonction constructeur
-  // Faire hériter le constructeur Animal au constructeur Oiseau
-  Animal.call(this, nombreDePattes, poids);
-  this.longueurDesAiles = longueurDesAiles;
-}
-// Faire hériter le prototype du constructeur Animal au constructeur Oiseau
-// Utiliser le prototype de notre constructeur hérité
-Oiseau.prototype = Object.create(Animal.prototype);
-// Rajouter la propriété constructor supprimée à la ligne du dessus
-Oiseau.prototype.constructor = Oiseau;
+let gandalf = { titre: "Magicien" };
 
-// Mammifere(nombreDePattes, poids / typeDePoils)
-function Mammifere(nombreDePattes, poids, typeDePoils) {
-  Animal.call(this, nombreDePattes, poids); // 1er paramètre => l'objet actuel (this) // 2e paramètre => les arguments pour son constructeur
-  this.typeDePoils = typeDePoils;
+function direBonjour() {
+  console.log("Bonjour " + this.titre);
 }
-
-var perroquet = new Oiseau(2, "1kg", "grandes");
-console.log(perroquet);
+// Appeler l'objet gandalf pour le transmettre à direBonjour()
+direBonjour.call(gandalf);
