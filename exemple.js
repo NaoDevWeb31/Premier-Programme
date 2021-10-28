6bdffddeabb876755f2874c98dc339f3ac511218
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 18:35:16 +0200

    [MAJ] Programmation orientée objet - Comprendre l'héritage v3

--- exemple.js
+++ exemple.js
@@ -8,14 +8,17 @@ function Animal(nombreDePattes, poids) {
 }
 
 // Oiseau(nombreDePattes, poids / longueurDesAiles)
-function Oiseau(longueurDesAiles) {
+function Oiseau(nombreDePattes, poids, longueurDesAiles) {
+  // La fonction Call() permet d'appeler une fonction constructeur
+  Animal.call(this, nombreDePattes, poids);
   this.longueurDesAiles = longueurDesAiles;
 }
 
 // Mammifere(nombreDePattes, poids / typeDePoils)
-function Mammifere(typeDePoils) {
+function Mammifere(nombreDePattes, poids, typeDePoils) {
+  Animal.call(this, nombreDePattes, poids); // 1er paramètre => l'objet actuel (this) // 2e paramètre => les arguments pour son constructeur
   this.typeDePoils = typeDePoils;
 }
 
-var perroquet = new Oiseau("grandes");
+var perroquet = new Oiseau(2, "1kg", "grandes");
 console.log(perroquet);
