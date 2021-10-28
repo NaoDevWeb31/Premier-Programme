1be2f4854df89dd1d6aba56dfbd6dff3ec9b0f37
Naomi Mango <naomi.mango@gmail.com>
2021-10-28 15:20:30 +0200

    [MAJ] Projet 4 - Étape 4

--- Projet_4-Formulaires_Le_juste_prix/code.js
+++ Projet_4-Formulaires_Le_juste_prix/code.js
@@ -10,6 +10,15 @@ error.style.display = "none";
 let nombreAleatoire = Math.floor(Math.random() * 1001);
 
 // Étape 4 - Vérifier que l'utilisateur donne bien un nombre
+input.addEventListener("keyup", () => {
+  if (isNaN(input.value)) {
+    // Afficher le message d'erreur
+    error.style.display = "inline";
+  } else {
+    // Cacher le message d'erreur
+    error.style.display = "none";
+  }
+});
 
 // Étape 5 - Agir à l'envoi du formulaire
 
