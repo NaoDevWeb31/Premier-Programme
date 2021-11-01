0f2643d451539bafcc3c3efd2f900db0d74b28ff
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 14:48:43 +0100

    [MAJ] Utiliser les API - Envoyer des données v2

--- exemple.js
+++ exemple.js
@@ -6,9 +6,15 @@ const url = "https://lesoublisdelinfo.com/api.php";
 let requete = new XMLHttpRequest();
 
 // GET
-requete.open("GET", url);
+// requete.open("GET", url);
+// requete.responseType = "json";
+// requete.send();
+
+// POST
+requete.open("POST", url);
+requete.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 requete.responseType = "json";
-requete.send();
+requete.send("prenom=John"); // pour envoyer plusieurs paramètres => prenom=John&nom=Doe
 
 requete.onload = function () {
   if (requete.readyState === XMLHttpRequest.DONE) {
