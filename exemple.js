37ef187e9b22ed33271c6cc765f80788b317d94e
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 14:25:00 +0100

    [MAJ] Utiliser les API - Récupérer le prix du bitcoin (XMLHttpRequest) v1

--- exemple.js
+++ exemple.js
@@ -1,2 +1,10 @@
 //                              UTILISER LES API
 /****** Récupérer le prix du bitcoin (XMLHttpRequest) ******/
+
+const url = "https://blockchain.info/ticker";
+
+// Créer une requête
+let requete = new XMLHttpRequest(); // Créer un objet
+requete.open("GET", url); // 1er paramètre GET/POST // 2e paramètre : url
+requete.responseType = "json"; // Nous attendons du JSON
+requete.send(); // Nous envoyons la requête
