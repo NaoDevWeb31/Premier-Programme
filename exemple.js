77294ff851db13264c247412802515de8033f9e3
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 12:39:11 +0100

    [MAJ] Devenez maître du temps - Récupérer la date actuelle v2

--- exemple.js
+++ exemple.js
@@ -2,5 +2,8 @@
 /****** Récupérer la date actuelle ******/
 
 // 1er janvier 1970 à minuit UTC (londres)
-let dateActuelle = Date(); // sous format textuel
-console.log(dateActuelle);
+let dateActuelle = Date(); // sous format textuel, norme anglo-saxonne
+console.log(dateActuelle); // Mon Nov 01 2021 12:38:00 GMT+0100 (Central European Standard Time)
+
+let dateEnSecondes = Date.now();
+console.log(dateEnSecondes); // 1635766680923
