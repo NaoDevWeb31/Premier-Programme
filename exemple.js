0cfca7400f8e45792b2c4be41af5717424f2d155
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 13:54:05 +0100

    [MAJ] Utiliser les API - Récupérer le prix du bitcoin (XMLHttpRequest) v0

--- exemple.js
+++ exemple.js
@@ -1,22 +1,2 @@
-//                              DEVENEZ MAÎTRE DU TEMPS
-/****** Utiliser les "templates string" avec les dates ******/
-
-// Avant
-// let prenom = "John";
-// let bonjour = "Bonjour " + prenom;
-// console.log(bonjour);
-
-// Maintenant
-// let prenom = "John";
-// let bonjour = `Bonjour ${prenom}`; // `template string utilisé ${variable}`
-// console.log(bonjour);
-
-// Avec les dates
-// let date = new Date().getUTCFullYear();
-// let copyright = `${date} © Believemy`; // Utile pour la mise à jour automatique de la date
-// console.log(copyright); // 2021 © Believemy
-
-// Ou encore plus pratique : des calculs
-let aliments = { fruits: 5, legumes: 1, biscuits: 75 };
-let panier = `Dans votre panier, vous avez ${aliments.fruits + aliments.legumes + aliments.biscuits} articles.`;
-console.log(panier); // Dans votre panier, vous avez 81 articles.
+//                              UTILISER LES API
+/****** Récupérer le prix du bitcoin (XMLHttpRequest) ******/
