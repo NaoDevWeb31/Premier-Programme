b684b317007a99190c5fa2074eff6146f44deb7f
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 14:25:55 +0100

    [MAJ] Utiliser les API - Récupérer le prix du bitcoin (XMLHttpRequest) v2

--- exemple.js
+++ exemple.js
@@ -8,3 +8,15 @@ let requete = new XMLHttpRequest(); // Créer un objet
 requete.open("GET", url); // 1er paramètre GET/POST // 2e paramètre : url
 requete.responseType = "json"; // Nous attendons du JSON
 requete.send(); // Nous envoyons la requête
+
+// Dès qu'on reçoit une réponse, cette fonction est exécutée
+requete.onload = function () {
+  // Quand la requête est terminée
+  if (requete.readyState === XMLHttpRequest.DONE) {
+    // Si la requête a été traitée avec succès
+    if (requete.status === 200) {
+      let reponse = requete.response; // On stocke la réponse
+      console.log(reponse);
+    }
+  }
+};
