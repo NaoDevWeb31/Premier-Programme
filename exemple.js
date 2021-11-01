45f9f1f1a2367b81e29befbdd5f00117a98155bf
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 13:21:10 +0100

    [MAJ] Devenez maître du temps - Les templates string avec les dates v4

--- exemple.js
+++ exemple.js
@@ -12,6 +12,11 @@
 // console.log(bonjour);
 
 // Avec les dates
-let date = new Date().getUTCFullYear();
-let copyright = `${date} © Believemy`; // Utile pour la mise à jour automatique de la date
-console.log(copyright); // 2021 © Believemy
+// let date = new Date().getUTCFullYear();
+// let copyright = `${date} © Believemy`; // Utile pour la mise à jour automatique de la date
+// console.log(copyright); // 2021 © Believemy
+
+// Ou encore plus pratique : des calculs
+let aliments = { fruits: 5, legumes: 1, biscuits: 75 };
+let panier = `Dans votre panier, vous avez ${aliments.fruits + aliments.legumes + aliments.biscuits} articles.`;
+console.log(panier); // Dans votre panier, vous avez 81 articles.
