f702d89ab47748da7e1fff0e09542fc90b1a3055
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 18:03:17 +0100

    [MAJ] Requêtes asynchrones, Callbacks & Promises - Promesses v2

--- exemple.js
+++ exemple.js
@@ -6,10 +6,27 @@
 // Exemple : Discord(), app de discussion en ligne, permet de parler aussi par micro.
     // Utilise l'asynchrone pour ne pas bloquer tant que vous n'acceptez pas l'utilisation du micro.
 
-const promesse = new Promise((resolve, reject) => {
-    // Tâches asynchrones
+// const promesse = new Promise((resolve, reject) => {
+//     // Tâches asynchrones
 
-    // Promesse honorée : resolve()
+//     // Promesse honorée : resolve()
 
-    // Promesse rompue : reject()
-});
+//     // Promesse rompue : reject()
+// });
+
+function chargerScript(script) {
+    return new Promise((resolve, reject) => {
+        // Créer un nouvel élément
+        let element = document.createElement("script");
+        element.src = script;
+        document.head.append(element);
+
+        // Deux possibilités : resolve()
+        element.onload = () => resolve("Fichier " + script + " a été chargé");
+
+        // reject()
+        element.onerror = () => reject(new Error("Opération impossible pour le script " + script));
+    });
+}
+
+const promesse = chargerScript("test.js");
