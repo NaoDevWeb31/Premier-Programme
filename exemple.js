59bed6102f42f25cf97f47905ce3365bf96d37a6
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 13:18:04 +0100

    [MAJ] Devenez maître du temps - Les templates string avec les dates v3

--- exemple.js
+++ exemple.js
@@ -7,6 +7,11 @@
 // console.log(bonjour);
 
 // Maintenant
-let prenom = "John";
-let bonjour = `Bonjour ${prenom}`; // `template string utilisé ${variable}`
-console.log(bonjour);
+// let prenom = "John";
+// let bonjour = `Bonjour ${prenom}`; // `template string utilisé ${variable}`
+// console.log(bonjour);
+
+// Avec les dates
+let date = new Date().getUTCFullYear();
+let copyright = `${date} © Believemy`; // Utile pour la mise à jour automatique de la date
+console.log(copyright); // 2021 © Believemy
