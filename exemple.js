91b90874418622a84e867615bcc44f6ca697436c
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 13:08:16 +0100

    [MAJ] Devenez maître du temps - Transformer une date au format local v3

--- exemple.js
+++ exemple.js
@@ -8,11 +8,8 @@ let dateActuelle = new Date();
 // toLocaleString     = jour, mois, année, heure
 // ON CHOISI LES OPTIONS QU'ON VEUT
 
-// Récupérer la langue du navigateur
-console.log(navigator.language); // fr-FR
-
-// Récupérer la date au format français
-let dateLocale = dateActuelle.toLocaleString("fr-FR", {
+// Récupérer la date dans le format de langue locale du navigateur
+let dateLocale = dateActuelle.toLocaleString(navigator.language, {
   weekday: "long", //  long (toutes les lettres), short (3 premières lettres), narrow (première lettre)
   year: "numeric", // numeric (xxxx), 2-digit (xx)
   month: "long", // long, short, narrow, numeric, 2-digit
