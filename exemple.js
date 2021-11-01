432c696aae7bde0348b475d40ca455826a877861
Naomi Mango <naomi.mango@gmail.com>
2021-11-01 12:44:20 +0100

    [MAJ] Devenez maître du temps - L'objet Date v2

--- exemple.js
+++ exemple.js
@@ -1,7 +1,9 @@
 //                              DEVENEZ MAÎTRE DU TEMPS
 /****** Utiliser l'objet Date ******/
 
-let datePrecise = new Date();
+// Déclarer une date
+let datePrecise = new Date(2780, 11, 10, 9, 39); // Wed Dec 10 2780 09:39:00 GMT+0100 (Central European Standard Time)
+console.log(datePrecise);
 // Au-dessus :
 //   - année (obligatoire)
 //   - mois (obligatoire) qui commence par 0
