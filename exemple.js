d555ca00407755a2464d3e320af9e259d1797ebe
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 16:16:23 +0100

    [MAJ] Les inclassables - Créer des modules (import, export) v2

--- exemple.js
+++ exemple.js
@@ -4,9 +4,11 @@
 // Attention : impossible d'utiliser les modules si vous n'avez pas de serveur (https), la plupart des navigateurs bloquent les modules pour notre sécurité s'ils ne sont pas exécutés sur des serveurs
 
 // index.js
-import { direBonjour } from './modules/direBonjour.js';
+import { direBonjour } from "./modules/direBonjour.js";
+
+direBonjour("John"); // John
 
 // modules/direBonjour.js
-export direBonjour(prenom) {
+export function direBonjour(prenom) {
   console.log("Bonjour " + prenom);
 }
