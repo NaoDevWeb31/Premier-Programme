24993772d9ddf520e907adf81a4829aca8c8034b
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:19:55 +0100

    [MAJ] jQuery - Sélectionner un élément avec JS v2

--- exemple.js
+++ exemple.js
@@ -3,6 +3,8 @@
 
 // Avec JavaScript
 document.querySelector("h1");
+document.querySelectorAll("p");
 
 // Avec jQuery
 $("h1"); // On donne le sélecteur CSS
+$("p"); // On sélectionne tous les paragraphes
