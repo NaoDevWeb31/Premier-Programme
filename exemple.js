e6cdb81594161d1d02f7c49d13666d4c7645a6de
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 16:21:10 +0100

    [MAJ] Les inclassables - Le mode strict v2

--- exemple.js
+++ exemple.js
@@ -2,4 +2,7 @@
 /****** Utiliser le mode strict ******/
 
 // Le mode strict : toujours placé au début (page ou fonction)
-"use strict";
+// "use strict";
+
+prenom = "John"; // Erreur silencieuse => affiche John
+console.log(prenom);
