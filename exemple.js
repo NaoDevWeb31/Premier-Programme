6a5d7327f0e2b2adf517a9854844d3caf68557e0
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 16:23:28 +0100

    [MAJ] Les inclassables - Le mode strict v4

--- exemple.js
+++ exemple.js
@@ -2,7 +2,15 @@
 /****** Utiliser le mode strict ******/
 
 // Le mode strict : toujours placé au début (page ou fonction)
-"use strict";
+// "use strict";
 
-prenom = "John"; // Uncaught ReferenceError: prenom is not defined
-console.log(prenom);
+prenom = "John";
+console.log(prenom); // erreur silencieuse => affiche John
+
+direBonjour();
+
+function direBonjour() {
+  "use strict";
+  nom = "Doe";
+  console.log(nom); // exemple.js:14 Uncaught ReferenceError: nom is not defined
+}
