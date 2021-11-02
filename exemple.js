e4a0f62d326f3ee018dfb2d8c8e457d3d74efe03
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 11:54:11 +0100

    [MAJ] Stocker des données dans le navigateur - Les cookies v3

--- exemple.js
+++ exemple.js
@@ -8,4 +8,10 @@
 document.cookie = "prenom=John";
 
 // Afficher tous les cookies
-alert(document.cookie);
+// alert(document.cookie);
+
+// Modifier un cookie
+document.cookie = "prenom=Mark";
+
+// Supprimer un cookie
+document.cookie = "prenom=";
