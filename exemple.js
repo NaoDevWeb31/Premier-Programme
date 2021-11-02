0198e0a0a1ede173009c18e4fac1da5f099663d6
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:41:37 +0100

    [MAJ] jQuery - Ajouter et supprimer un élément avec JS v2

--- exemple.js
+++ exemple.js
@@ -3,8 +3,10 @@
 
 // Avec JavaScript
 document.querySelector("h1").prepend("Oh : "); // Ajouter devant avec JS
+document.querySelector("h1").append(" !"); // Ajouter après avec JS
 document.querySelectorAll("p");
 
 // Avec jQuery
 $("h1").prepend("Oh : "); // Ajouter devant avec jQuery
+$("h1").append(" !"); // Ajouter après avec jQuery
 $("p");
