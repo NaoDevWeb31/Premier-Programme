8c5f142ad611ddcb5bff319ddfec565a6c9c270e
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 15:39:06 +0100

    [MAJ] jQuery - Ajouter des animations avec JS v8

--- exemple.js
+++ exemple.js
@@ -8,5 +8,9 @@ $("h1").show(); // afficher le titre
 $("p").click(() => {
   // $("h1").toggle(); // cacher ou afficher le titre
   // $("h1").fadeToggle(); // cacher ou afficher le titre avec un délai de transition
-  $("h1").slideToggle(); // cacher ou afficher le titre avec un effet de pliement/dépliement
+  // $("h1").slideToggle(); // cacher ou afficher le titre avec un effet de pliement/dépliement
+  $("h1").animate({
+    opacity: 0.7,
+    margin: 30,
+  });
 });
