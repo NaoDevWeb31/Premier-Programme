5c292c9e2383b90d983b6b36ee8b21d60d5cc526
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 15:57:13 +0100

    [MAJ] Les inclassables - Géolocalisation v1

--- exemple.js
+++ exemple.js
@@ -1,20 +1,19 @@
-//                              ET SI ON ÉCRIVAIT PLUS VITE AVEC JQUERY ?
-/****** Ajouter des animations avec jQuery et JavaScript ******/
+//                              LES INCLASSABLES
+/****** Géolocaliser un utilisateur ******/
 
-$("h1").hide(); // cacher le titre
-$("h1").show(); // afficher le titre
+// Pour géolocaliser on utilise l'objet geolocation
 
-// au clic d'un paragraphe
-$("p").click(() => {
-  // $("h1").toggle(); // cacher ou afficher le titre
-  // $("h1").fadeToggle(); // cacher ou afficher le titre avec un délai de transition
-  // $("h1").slideToggle(); // cacher ou afficher le titre avec un effet de pliement/dépliement
-  /* $("h1").animate({
-    opacity: 0.7,
-    margin: 30,
-  }); */
-  $("h1").slideUp().slideDown().animate({
-    opacity: 0.7,
-    margin: 30,
+// Vérifier que la géolocalisation soit disponible
+if ("geolocation" in navigator) {
+  // getCurrentPosition() retourne une fois la position de l'utilisateur
+                                    // OU
+  // watchPosition() actualise la position plusieurs fois selon un délai variable
+                    // 3 paramètres : success, error, options
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
   });
-});
+
+} else {
+  alert("La géolocalisation ne peut pas être utilisée.");
+}
