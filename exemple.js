dbceab2f7bb37c5d528204aa519d69723d72c0f2
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 11:40:19 +0100

    [MAJ] Améliorons nos requêtes avec Fetch & Axios - Envoyer des données avec Axios v1

--- exemple.js
+++ exemple.js
@@ -1,54 +1,56 @@
 //                              AMÉLIORONS NOS REQUÊTES AVEC FETCH & AXIOS
-/****** Récupérer des données avec Axios ******/
+/****** Envoyer des données avec Axios ******/
+const url = "https://lesoublisdelinfo.com/api.php";
 
-const url = "https://blockchain.info/ticker";
+// let requete = new XMLHttpRequest();
 
-async function recupererPrix() {
-  // // Créer une requête
-  // let requete = new XMLHttpRequest(); // Créer un objet
-  // requete.open("GET", url); // 1er paramètre GET/POST // 2e paramètre : url
-  // requete.responseType = "json"; // Nous attendons du JSON
-  // requete.send(); // Nous envoyons la requête
+// GET
+// requete.open("GET", url);
+// requete.responseType = "json";
+// requete.send();
 
-  // // Dès qu'on reçoit une réponse, cette fonction est exécutée
-  // requete.onload = function () {
-  //   // Quand la requête est terminée
-  //   if (requete.readyState === XMLHttpRequest.DONE) {
-  //     // Si la requête a été traitée avec succès
-  //     if (requete.status === 200) {
-  //       let reponse = requete.response; // On stocke la réponse
-  //       let prixEnEuros = reponse.EUR.last;
-  //       document.querySelector("#price_label").textContent = prixEnEuros;
-  //     } else {
-  //       alert("Un problème est survenue, merci de revenir plus tard.");
-  //     }
-  //   }
-  // };
-  // console.log("Prix actualisé");
+// POST
+// requete.open("POST", url);
+// requete.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// requete.responseType = "json";
+// requete.send("prenom=John"); // pour envoyer plusieurs paramètres => prenom=John&nom=Doe
 
-  // const requete = await fetch(url, {
-  //   method: "GET",
-  // });
+// requete.onload = function () {
+//   if (requete.readyState === XMLHttpRequest.DONE) {
+//     if (requete.status === 200) {
+//       let reponse = requete.response;
+//       console.log(reponse);
+//     } else {
+//       alert("Un problème est intervenu, merci de revenir plus tard.");
+//     }
+//   }
+// };
 
-  // if (!requete.ok) {
-  //   alert("Un problème est survenue.");
-  // } else {
-  //   let donnees = await requete.json();
-  //   // console.log(donnees);
-  //   document.querySelector("span").textContent = donnees.EUR.last;
-  // }
+// async function envoyerPrenom(prenom) {
+//   const requete = await fetch(url, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: new URLSearchParams({
+//       prenom,
+//     }),
+//   });
 
-  axios.get(url)
-  .then(function(donnees){
-    // console.log(donnees);
-    document.querySelector("span").textContent = donnees.data.EUR.last;
+//   if (!requete.ok) {
+//     alert("Un problème est survenue.");
+//   } else {
+//     const donnees = await requete.json();
+//     console.log(donnees);
+//   }
+// }
+
+// envoyerPrenom("Elon");
+
+axios.post(url, {})
+  .then(function (donnees) {
+    console.log(donnees);
   })
-  .catch(function(erreur){
-    alert("Un problème est survenue.");
-  })
-  .then(function(){
-    console.log("Mise à jour effectuée");
+  .catch(function (erreur) {
+    console.log(erreur);
   });
-}
-
-setInterval(recupererPrix, 1000);
