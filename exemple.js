9582cc9338e2f545bede4dd69b779377bd254207
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 15:03:06 +0100

    [MAJ] jQuery - Ajouter des évènements avec JS v1

--- exemple.js
+++ exemple.js
@@ -1,14 +1,14 @@
 //                              ET SI ON ÉCRIVAIT PLUS VITE AVEC JQUERY ?
-/****** Modifier les attributs d'un élément avec jQuery et JavaScript ******/
+/****** Ajouter des évènements avec jQuery et JavaScript ******/
 
 // Avec JavaScript
-document.querySelector("h1").getAttribute("class"); // title
-// document.querySelector("h1").setAttribute("style", "text-transform: lowercase");
+/* document.querySelector("h1").addEventListener("click", () => {
+  document.querySelector("h1").style.color = "orange";
+}); */
 document.querySelectorAll("p");
 
 // Avec jQuery
-$("h1").attr("class"); // sélectionner un attribut
-$("h1").attr("style", "text-transform: lowercase"); // modifier un attribut
-$("h1").after(`<a href="https://wikipedia.com">Wikipedia</a>`); // ajouter un lien après le titre h1
-$("a").attr("href", "https://believemy.com"); // modifier l'url de l'élément lien ajouté
+$("h1").click(() => {
+  $("h1").css("color", "orange"); // au clic, le titre devient orange
+});
 $("p");
