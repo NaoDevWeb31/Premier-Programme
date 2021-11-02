b482fc577286e9e40e586a193fa5748943f429eb
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:49:59 +0100

    [MAJ] jQuery - Modifier les attributs d'un élément avec JS v1

--- exemple.js
+++ exemple.js
@@ -1,14 +1,10 @@
 //                              ET SI ON ÉCRIVAIT PLUS VITE AVEC JQUERY ?
-/****** Ajouter et supprimer un élément avec jQuery et JavaScript ******/
+/****** Modifier les attributs d'un élément avec jQuery et JavaScript ******/
 
 // Avec JavaScript
-document.querySelector("h1").prepend("Oh : "); // Ajouter devant avec JS
-document.querySelector("h1").append(" !"); // Ajouter après avec JS
+document.querySelector("h1").getAttribute("class"); // title
 document.querySelectorAll("p");
 
 // Avec jQuery
-$("h1").prepend("Oh : "); // Ajouter devant avec jQuery
-$("h1").append(" !"); // Ajouter après avec jQuery
-$("h1").before("<div>Hello</div>"); // Nouveau div avant le titre h1
-$("h1").after("<div>Hello</div>"); // Nouveau div après le titre h1
-$("p").remove(); // Supprimer les paragraphes avec jQuery
+$("h1").attr("class"); // sélectionner un attribut
+$("p");
