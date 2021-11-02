580e3ed5d5c3de44337a6350c908e1f51228e301
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:33:39 +0100

    [MAJ] jQuery - Modifier le style d'un élément avec JS v2

--- exemple.js
+++ exemple.js
@@ -3,8 +3,10 @@
 
 // Avec JavaScript
 document.querySelector("h1").style.color = "orange";
+// document.querySelector("#monId").className = "important";
 document.querySelectorAll("p");
 
 // Avec jQuery
 $("h1").css("color", "blue");
+$("#monId").addClass("important");
 $("p");
