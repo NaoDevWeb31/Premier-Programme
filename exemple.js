5c1f847d9b13725ded9253f812f1f2c77fadf35d
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:12:49 +0100

    [MAJ] jQuery - Installation

--- exemple.js
+++ exemple.js
@@ -1,24 +1,2 @@
-//                              STOCKER DES DONNÉES DANS LE NAVIGATEUR AVEC JAVASCRIPT
-/****** LocalStorage & SessionStorage ******/
-
-// LocalStorage = Stocké dans le navigateur MÊME APRÈS qu'il soit fermé => conseillé
-// SessionStorage = Stocké dans le navigateur PENDANT la session
-// Jamais envoyé au serveur contrairement aux cookies
-// Plus simple à utiliser qu'un cookie
-
-// Et les cookies ?
-
-// setItem[clé], [valeur]) // getItem[clé] // removeItem[clé]
-// clear() : supprimer tout
-// key([index]) : obtenir la clé située à l'index donnée
-// length : obtenir le nombre d'éléments stockés
-
-if (localStorage.getItem("prenom")) {
-  document.body.append("Bonjour " + localStorage.getItem("prenom"));
-} else {
-  let prenom = prompt("Quel est votre prénom ?");
-  localStorage.setItem("prenom", prenom);
-  document.body.append(prenom);
-}
-
-localStorage.clear();
+//                              ET SI ON ÉCRIVAIT PLUS VITE AVEC JQUERY ?
+/****** jQuery ******/
