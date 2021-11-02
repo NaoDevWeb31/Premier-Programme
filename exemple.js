05f3efbcd913e9e997cf035c9c3402e3be976d86
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 12:22:45 +0100

    [MAJ] Stocker des données dans le navigateur - LocalStorage & SessionStorage v1

--- exemple.js
+++ exemple.js
@@ -1,22 +1,14 @@
 //                              STOCKER DES DONNÉES DANS LE NAVIGATEUR AVEC JAVASCRIPT
-/****** Fiche technique sur les cookies et HttpOnly ******/
+/****** LocalStorage & SessionStorage ******/
 
-Ensemble des paramètres pour les cookies
+// LocalStorage = Stocké dans le navigateur MÊME APRÈS qu'il soit fermé => conseillé
+// SessionStorage = Stocké dans le navigateur PENDANT la session
+  // Jamais envoyé au serveur contrairement aux cookies
+  // Plus simple à utiliser qu'un cookie
 
-◉ path ➣ Permet de préciser sur lequel répertoire (dossier) est disponible le cookie
+// Et les cookies ?
 
-◉ domain ➣ Permet de préciser sur quel nom de domaine est disponible le cookie
-
-◉ expires ➣ Permet de donner une date UTC jusqu'à laquelle le cookie est disponible, après cette date, il expirera
-
-◉ max-age ➣ Se substitue à expires, permet de donner une durée en millisecondes jusqu'à laquelle le cookie est disponible. Après cette date, il expirera
-
-◉ secure ➣ Permet de n'autoriser l'utilisation du cookie que sur les sites sécurisés (https)
-
-
-
-🆕 L'erreur HttpOnly
-Pour créer un cookie, le serveur utilise une fonction qui possède, parfois, une option ⟹ httpOnly.
-❎ Cette option interdit tout simplement la lecture / modification / suppression de ce cookie par JavaScript. 
-Je vous rassure, si c'est vous qui programmez votre site, vous saurez à l'avance ce que vous faites.
-‼️ Mais il est bon de rappeler dans cette session que si vous mettez l'option httpOnly lorsque vous créez vos cookies via un autre langage de programmation (comme le PHP), votre cookie ne pourra pas être utilisé par JavaScript. Maintenant, vous le savez !
+// setItem[clé], [valeur]) // getItem[clé] // removeItem[clé]
+// clear() : supprimer tout
+// key([index]) : obtenir la clé située à l'index donnée
+// length : obtenir le nombre d'éléments stockés
