08a0323dd8d1e73f8ee6a8a09d4843aa0a513c0d
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:58:30 +0100

    [MAJ] jQuery - Modifier les attributs d'un élément avec JS v2

--- exemple.js
+++ exemple.js
@@ -3,8 +3,12 @@
 
 // Avec JavaScript
 document.querySelector("h1").getAttribute("class"); // title
+// document.querySelector("h1").setAttribute("style", "text-transform: lowercase");
 document.querySelectorAll("p");
 
 // Avec jQuery
 $("h1").attr("class"); // sélectionner un attribut
+$("h1").attr("style", "text-transform: lowercase"); // modifier un attribut
+$("h1").after(`<a href="https://wikipedia.com">Wikipedia</a>`); // ajouter un lien après le titre h1
+$("a").attr("href", "https://believemy.com"); // modifier l'url de l'élément lien ajouté
 $("p");
