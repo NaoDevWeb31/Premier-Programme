266158edac50cc6725aeae8e8b4954f86b9c1c00
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:29:57 +0100

    [MAJ] jQuery - Modifier le style d'un élément avec JS v1

--- exemple.js
+++ exemple.js
@@ -1,14 +1,10 @@
 //                              ET SI ON ÉCRIVAIT PLUS VITE AVEC JQUERY ?
-/****** Modifier le contenu d'un élément avec jQuery et JavaScript ******/
+/****** Modifier le style d'un élément avec jQuery et JavaScript ******/
 
 // Avec JavaScript
-document.querySelector("h1").textContent = "Bonjour avec JavaScript";
-// document.querySelectorAll("p").textContent = "Ceci est un paragraphe"; // Impossible
-document.querySelector(
-  ".maClass"
-).innerHTML = `<p><strong>HTML modifié avec JavaScript</strong></p>`;
+document.querySelector("h1").style.color = "orange";
+document.querySelectorAll("p");
 
 // Avec jQuery
-$("h1").text("Bonjour avec jQuery"); // On donne le sélecteur CSS
-// $("p").text("Ceci est un paragraphe"); // On sélectionne tous les paragraphes
-$(".maClass").html("<p><strong>HTML modifié avec jQuery</strong></p>");
+$("h1").css("color", "blue");
+$("p");
