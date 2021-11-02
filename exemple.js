251d865d98114ceba08ba5894a19227729822dfc
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:40:21 +0100

    [MAJ] jQuery - Ajouter et supprimer un élément avec JS v1

--- exemple.js
+++ exemple.js
@@ -1,14 +1,10 @@
 //                              ET SI ON ÉCRIVAIT PLUS VITE AVEC JQUERY ?
-/****** Modifier le style d'un élément avec jQuery et JavaScript ******/
+/****** Ajouter et supprimer un élément avec jQuery et JavaScript ******/
 
 // Avec JavaScript
-document.querySelector("h1").style.color = "orange";
-// document.querySelector("#monId").className = "important";
-// document.querySelector("#monId").className = ""; // Impossible de supprimer une classe avec JS
-// document.querySelectorAll("p").style.color = "purple"; // Impossible de colorer tous les paragraphes d'un coup
+document.querySelector("h1").prepend("Oh : "); // Ajouter devant avec JS
+document.querySelectorAll("p");
 
 // Avec jQuery
-$("h1").css("color", "blue");
-$("#monId").addClass("important");
-$("#monId").removeClass("important");
-$("p").css("color", "purple");
+$("h1").prepend("Oh : "); // Ajouter devant avec jQuery
+$("p");
