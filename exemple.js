d14180a73c48166d30f4c34395300731fee4e3c1
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 16:20:06 +0100

    [MAJ] Les inclassables - Le mode strict v1

--- exemple.js
+++ exemple.js
@@ -1,17 +1,5 @@
 //                              LES INCLASSABLES
-/****** Créer des modules (import, export) ******/
+/****** Utiliser le mode strict ******/
 
-// Attention : impossible d'utiliser les modules si vous n'avez pas de serveur (https), la plupart des navigateurs bloquent les modules pour notre sécurité s'ils ne sont pas exécutés sur des serveurs
-
-// index.js
-import { direBonjour } from "./modules/direBonjour.js";
-
-direBonjour("John"); // John
-
-// modules/direBonjour.js
-export function direBonjour(prenom) {
-  console.log("Bonjour " + prenom);
-}
-
-// index.html
-<script type="module" src="index.js"></script>;
+// Le mode strict : toujours placé au début (page ou fonction)
+"use strict";
