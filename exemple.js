24f7bc3b625521447b9b16ceaab280a7b4635432
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 15:06:56 +0100

    [MAJ] jQuery - Ajouter des évènements avec JS v2

--- exemple.js
+++ exemple.js
@@ -5,10 +5,16 @@
 /* document.querySelector("h1").addEventListener("click", () => {
   document.querySelector("h1").style.color = "orange";
 }); */
-document.querySelectorAll("p");
+/* for (let i = 0; i < 2; i++) {
+  document.querySelectorAll("p")[i].addEventListener("click", () => {
+    document.querySelector("h1").style.color = "orange";
+  });
+} */
 
 // Avec jQuery
 $("h1").click(() => {
   $("h1").css("color", "orange"); // au clic, le titre devient orange
 });
-$("p");
+$("p").click(() => {
+  $("h1").css("color", "orange"); // au clic d'un des paragraphes, le titre devient orange
+});
