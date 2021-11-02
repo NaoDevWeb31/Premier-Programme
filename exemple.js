b08bdfb72d21bad144785c90d0911a5b1a4b676b
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 14:23:32 +0100

    [MAJ] jQuery - Modifier le contenu d'un élément avec JS v2

--- exemple.js
+++ exemple.js
@@ -3,8 +3,8 @@
 
 // Avec JavaScript
 document.querySelector("h1").textContent = "Bonjour avec JavaScript";
-document.querySelectorAll("p");
+// document.querySelectorAll("p").textContent = "Ceci est un paragraphe"; // Impossible
 
 // Avec jQuery
 $("h1").text("Bonjour avec jQuery"); // On donne le sélecteur CSS
-$("p"); // On sélectionne tous les paragraphes
+$("p").text("Ceci est un paragraphe"); // On sélectionne tous les paragraphes
