8cd2bdb46e92cbf149d6e4fed23e56471b06b17e
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 15:33:24 +0100

    [MAJ] jQuery - Ajouter des animations avec JS v6

--- exemple.js
+++ exemple.js
@@ -2,10 +2,10 @@
 /****** Ajouter des animations avec jQuery et JavaScript ******/
 
 $("h1").hide(); // cacher le titre
-// $("h1").show(); // afficher le titre
+$("h1").show(); // afficher le titre
 
 // au clic d'un paragraphe
 $("p").click(() => {
   // $("h1").toggle(); // cacher ou afficher le titre
-  $("h1").fadeIn(); // afficher le titre avec un délai de transition
+  $("h1").fadeToggle(); // cacher ou afficher le titre avec un délai de transition
 });
