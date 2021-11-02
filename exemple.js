dc5fafe3905c0f87e6cd78d1625717bdd0745a0c
Naomi Mango <naomi.mango@gmail.com>
2021-11-02 12:05:04 +0100

    [MAJ] Stocker des données dans le navigateur - Cookies & HttpOnly - Résumé

--- exemple.js
+++ exemple.js
@@ -1,17 +1,22 @@
 //                              STOCKER DES DONNÉES DANS LE NAVIGATEUR AVEC JAVASCRIPT
-/****** Les cookies ******/
+/****** Fiche technique sur les cookies et HttpOnly ******/
 
-// Pour utiliser un cookie => avoir un serveur locale ou avoir un site internet
-// Simuler un serveur avec MAMP, WAMP, LAMP, XAMP
+Ensemble des paramètres pour les cookies
 
-// Créer un cookie
-document.cookie = "prenom=John";
+◉ path ➣ Permet de préciser sur lequel répertoire (dossier) est disponible le cookie
 
-// Afficher tous les cookies
-// alert(document.cookie);
+◉ domain ➣ Permet de préciser sur quel nom de domaine est disponible le cookie
 
-// Modifier un cookie
-document.cookie = "prenom=Mark";
+◉ expires ➣ Permet de donner une date UTC jusqu'à laquelle le cookie est disponible, après cette date, il expirera
 
-// Supprimer un cookie
-document.cookie = "prenom=";
+◉ max-age ➣ Se substitue à expires, permet de donner une durée en millisecondes jusqu'à laquelle le cookie est disponible. Après cette date, il expirera
+
+◉ secure ➣ Permet de n'autoriser l'utilisation du cookie que sur les sites sécurisés (https)
+
+
+
+🆕 L'erreur HttpOnly
+Pour créer un cookie, le serveur utilise une fonction qui possède, parfois, une option ⟹ httpOnly.
+❎ Cette option interdit tout simplement la lecture / modification / suppression de ce cookie par JavaScript. 
+Je vous rassure, si c'est vous qui programmez votre site, vous saurez à l'avance ce que vous faites.
+‼️ Mais il est bon de rappeler dans cette session que si vous mettez l'option httpOnly lorsque vous créez vos cookies via un autre langage de programmation (comme le PHP), votre cookie ne pourra pas être utilisé par JavaScript. Maintenant, vous le savez !
